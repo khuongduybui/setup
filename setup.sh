cc4f2fb90ca9dcceed572f7751ad65163682820f
Duy K. Bui <duy@buifamily.info>
2014-11-15 23:49:33 -0500

    Add clearer progress

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #! /bin/sh
+echo "Setting up global preferences..."
 echo 'export MESA_EXTENSION_OVERRIDE="-GL_EXT_texture_sRGB_decode -GL_ARB_draw_elements_base_vertex -GL_ARB_map_buffer_range"' >> /etc/profile.d/LoLFix.sh
 
 test -f ~/.bash_aliases && mv ~/.bash_aliases ~/.bash_aliases.bak
 ln -s ~/setup/.bash_aliases ~/
 test -d ~/bin || mkdir ~/bin
 
-which apt-get && sh ~/setup/ubuntu.sh
+which apt-get && echo "Ubuntu detected. Installing core modules..." && sh ~/setup/ubuntu.sh > /dev/null
 
 chmod +x bin/*.sh
 test -d ~/code || mkdir ~/code
 test -d ~/code/test || mkdir ~/code/test
 
-npm install -g mocha express-generator nodemon
+echo "Installing NPM tools..."
+npm install -g mocha express-generator nodemon  > /dev/null
 
 echo ''
 echo '=== Additional modules ==='
