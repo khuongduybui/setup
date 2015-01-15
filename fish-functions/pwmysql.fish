5187fed4c4be4a3cb9509af6e79f03b328206a41
Duy K. Bui <duybui@amazon.com>
2015-01-15 14:44:39 -0500

    Add elementary extras

--- fish-functions/pwmysql.fish
+++ fish-functions/pwmysql.fish
@@ -1,9 +1,7 @@
 function pwmysql
-	set e (pwe);
-    set pMaterial (grep -e '<root source="odin"' apollo-overrides/$e/mysql-config/server.xml | tr '< >' '\n' | grep materialset | sed -e 's/materialset="//' -e 's/"//' -);
-      set pw (odin $pMaterial)
-        set db (grep -e '<database name=' apollo-overrides/$e/mysql-config/server.xml | tr '< >' '\n' | grep name | sed -e 's/name="//' -e 's/"//' -);
-
-	  ape mysql -S /apollo/env/$e/var/mysql/state/mysql.sock -u root --password=$pw $db
-
+	set e (pwe)
+	set pMaterial (grep -e '<root source="odin"' apollo-overrides/$e/mysql-config/server.xml | tr '< >' '\n' | grep materialset | sed -e 's/materialset="//' -e 's/"//' -)
+	set pw (odin $pMaterial)
+	set db (grep -e '<database name=' apollo-overrides/$e/mysql-config/server.xml | tr '< >' '\n' | grep name | sed -e 's/name="//' -e 's/"//' -)
+	ape mysql -S /apollo/env/$e/var/mysql/state/mysql.sock -u root --password=$pw $db
 end
