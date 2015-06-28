74ed3fb83f39db74de3ae6b802ff48a5613db76e
Duy K. Bui <duy@buifamily.info>
2015-06-28 19:16:26 -0400

    Surface winps

---
+++ WindowsPowerShell/Scripts/surface.ps1
@@ -0,0 +1,1 @@
+Enter-PSSession -ComputerName IAD219901243603 -credential (new-object -typename System.Management.Automation.PSCredential -argumentlist "ANT\duybui", (ConvertTo-SecureString "Honeytrungga=141592" -AsPlainText -Force)) -EnableNetworkAccess
