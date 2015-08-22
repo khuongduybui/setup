e9812a55784d2a25f4596af6021d940918c909f4
Duy K. Bui <duybui@amazon.com>
2015-08-21 22:23:15 -0400

    Even more minimal

--- WindowsPowerShell/Scripts/environment-init.ps1
+++ WindowsPowerShell/Scripts/environment-init.ps1
@@ -13,32 +13,10 @@ $env:HOME = Resolve-Path ("~");
 
 Wrap-Ls;
 
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted > $null;
-Set-PackageSource -Name chocolatey -Trusted > $null;
-
-Write-Host "Verifying PowerShellGet modules..." -ForegroundColor Yellow;
-Verify-PS-Module "Pscx";
-Verify-PS-Module "ShowUI";
-Verify-PS-Module "PsReadline";
-# if you don't already have this configured...
-Set-PSReadLineOption -HistoryNoDuplicates
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
-Set-PSReadLineOption -MaximumHistoryCount 4000
-# history substring search
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-# Tab completion
-Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-Write-Host "Verifying PsGet modules..." -ForegroundColor Yellow;
-$global:PsGetDestinationModulePath = "$(Split-Path $Profile)\Modules";
-Verify-Module "PsGet";
-Verify-Module "Find-String" "awk";
 $gitPath = (Verify-Command -o "git").Path;
 if ($gitPath -ne $null) {
   Verify-Path "$gitPath/../../bin";
+  Verify-Path "$gitPath/../../usr/bin";
   Verify-Module "posh-git";
   ssh-add "~/.ssh/openfisma-ec2.pem";
 }
