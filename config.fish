c1cfa175ebcce3a4874fb8f10cead5ad2286b6b9
Duy Bui <duybui@amazon.com>
2015-09-17 14:46:55 +0000

    Update fish

--- config.fish
+++ config.fish
@@ -43,4 +43,4 @@ set -g fish_user_abbreviations 'server=ape /apollo/env/SDETools/bin/brazil-build
 set -g fish_user_abbreviations 'pkg=ape /apollo/end/SDETools/bin/brazil-build apollo-pkg' $fish_user_abbreviations
 set -g fish_user_abbreviations 'esudo=sudo -E' $fish_user_abbreviations
 set -g fish_user_abbreviations 'eossh=ssh -i .ssh/id_eos_ec2 -l ec2-user' $fish_user_abbreviations
-status --is-interactive; and byobu-launcher
+status --is-login; and status --is-interactive; and exec byobu-launcher -S ~/.config/fish/my-session
--- rhel.sh
+++ rhel.sh
@@ -1,9 +1,9 @@
-mv ~/.zshrc ~/.zshrc.`date +%Y-%m-%d`.bak
-echo "/apollo/env/envImprovement/bin/fish" > ~/.zshrc
-scp duybui.aka.amazon.com:~/.ssh/id_rsa ~/.ssh/
-scp duybui.aka.amazon.com:~/.ssh/authorized_keys ~/.ssh/
-chmod 400 ~/.ssh/id_rsa
-git clone git@bitbucket.org:khuongduybui/linux.git ~/setup
+mv /home/duybui/.zshrc /home/duybui/.zshrc.`date +%Y-%m-%d`.bak
+echo "/apollo/env/envImprovement/bin/fish" > /home/duybui/.zshrc
+#scp duybui.aka.amazon.com:/home/duybui/.ssh/id_rsa /home/duybui/.ssh/
+#scp duybui.aka.amazon.com:/home/duybui/.ssh/authorized_keys /home/duybui/.ssh/
+#chmod 400 /home/duybui/.ssh/id_rsa
+#git clone git@bitbucket.org:khuongduybui/linux.git /home/duybui/setup
 
 wget ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/5/x86_64/tmux-1.4-3.el5.1.x86_64.rpm
 wget ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/5/x86_64/byobu-5.73-4.el5.noarch.rpm
@@ -11,14 +11,14 @@ sudo yum install tmux-1.4-3.el5.1.x86_64.rpm
 sudo yum install byobu-5.73-4.el5.noarch.rpm
 byobu-launcher-install
 
-exec byobu-launcher
+#exec byobu-launcher
 
 wget http://mirror.sfo12.us.leaseweb.net/epel/5/i386/epel-release-5-4.noarch.rpm
 sudo yum install http://mirror.sfo12.us.leaseweb.net/epel/5/i386/epel-release-5-4.noarch.rpm
 
-mkdir ~/code
+mkdir /home/duybui/code
 
-mkdir ~/opt
+mkdir /home/duybui/opt
 echo install iojs
 sudo yum install gcc gcc-c++
-mkdir ~/bin
+mkdir /home/duybui/bin
