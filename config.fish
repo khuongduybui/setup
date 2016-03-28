5fc69954ae93a1239341d2ab8b024b703e59b7cd
Duy K. Bui <duybui@amazon.com>
2016-03-28 13:22:00 -0400

    Update aps

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ if test -d /usr/local/Cellar/autojump
 	source /usr/local/Cellar/autojump/*/etc/autojump.fish
 end
 
-if test (which byobu-launcher) > /dev/null 2>&1
+if test (which byobu-screen) > /dev/null 2>&1
 end
 
 #Global
@@ -49,4 +49,4 @@ set -g fish_user_abbreviations $fish_user_abbreviations
 set -g fish_user_abbreviations 'pickaxe=ssh -At duybui-um.aka.amazon.com ssh -t aws-pickaxe-iad-prod-1a-i-24be46da.us-east-1.amazon.com /apollo/env/AWSPickaxeEscritoire/bin/pickaxe' $fish_user_abbreviations
 set -g fish_user_abbreviations 'grep-runtime-log=grep (brazil-bootstrap)/var/output/logs/* -e' $fish_user_abbreviations
 set -g fish_user_abbreviations 'rebuild-coral=clear; begin; set -lx SERVICE (pwe); cd ../"$SERVICE"Model/; brazil-build; cd ../"$SERVICE"Generator/; brazil-build; cd ../"$SERVICE"; brazil-build server; end' $fish_user_abbreviations
-status --is-login; and status --is-interactive; and exec byobu-launcher
+status --is-login; and status --is-interactive; and exec byobu-screen
