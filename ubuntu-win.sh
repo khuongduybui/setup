d7cc5928476fcca581675ad0dff4a6fefaa60d79
Duy K. Bui <duy@buifamily.info>
2016-04-07 17:28:43 -0400

    Install stuff on ubuntu-win

--- ubuntu-win.sh
+++ ubuntu-win.sh
@@ -1,1 +1,13 @@
-echo "test";
+#! /bin/sh
+sudo apt-add-repository -y ppa:fish-shell/release-2
+sudo apt-add-repository -y ppa:eugenesan/ppa
+sudo apt-add-repository -y ppa:neovim-ppa/unstable
+wget -O - https://deb.nodesource.com/setup_5.x | sudo bash - > /dev/null
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y \
+  fish \
+  neovim unison \
+  nodejs \
+  python-dev python-pip python3-dev python3-pip
