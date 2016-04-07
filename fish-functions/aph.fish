a637b90a32b2fa6ee696e78ed73b42fad5ec82e0
Duy K. Bui <duy@buifamily.info>
2016-04-07 16:28:10 -0400

    UNIX line ending

--- fish-functions/aph.fish
+++ fish-functions/aph.fish
@@ -2,6 +2,6 @@ function aph
 	if not test -x /apollo/bin/apolloHostControl
 		set_color $fish_color_error; echo "ERROR: Apollo not found."
 		return
-	end		
+	end
 	sudo /apollo/bin/apolloHostControl $argv
 end
