3f3df1877bb9049f433ce8f39737e90574ebca7f
Duy K. Bui <duy@buifamily.info>
2016-04-17 17:22:15 -0400

    Bypass code.cmd

--- WindowsPowerShell/Scripts/e.ps1
+++ WindowsPowerShell/Scripts/e.ps1
@@ -1,7 +1,7 @@
-$code = Test-Path "$env:programfiles (x86)/Microsoft VS Code/bin/code.cmd";
+$code = Test-Path "$env:programfiles (x86)/Microsoft VS Code/code.exe";
 if ($code -eq $true) {
-	$path = Resolve-Path "$env:programfiles (x86)/Microsoft VS Code/bin";
-	. "$path/code" $args;
+	$path = Resolve-Path "$env:programfiles (x86)/Microsoft VS Code/code.exe";
+	. "$path" $args;
 } else {
 	notepad $args;
 }
