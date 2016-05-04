a6664f0e530a337873a6f60fc71dcc6d858c87ae
Duy K. Bui <duybui@amazon.com>
2016-05-04 11:08:03 -0400

    better aps

--- fish-functions/aps.fish
+++ fish-functions/aps.fish
@@ -1,7 +1,7 @@
 function aps
 	if test -d /apollo/env/envImprovement
-		set -lx PATH /apollo/env/SDETools/bin $PATH
-		set -lx BRAZIL_PLATFORM_OVERRIDE RHEL5_64
-		/apollo/env/envImprovement/bin/fish
+		# set -lx PATH /apollo/env/SDETools/bin $PATH
+		# set -lx BRAZIL_PLATFORM_OVERRIDE RHEL5_64
+		/apollo/bin/env -e SDETools /apollo/bin/env -e WDETools /apollo/bin/env -e BarkCLI /apollo/bin/env -e envImprovement fish
 	end
 end
