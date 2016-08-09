2545b11c0ba698e7fadc7edd776498d57b061370
Duy K. Bui <duy@buifamily.info>
2016-08-09 15:08:20 -0400

    Node-Init recursive

--- WindowsPowerShell/Scripts/Node-Init.ps1
+++ WindowsPowerShell/Scripts/Node-Init.ps1
@@ -5,19 +5,12 @@ try {
   } catch {
     Write-Host "node missing. Attempting to install..." -ForegroundColor Yellow;
     nodist latest;
-    Write-Host "node $(node --version)" -ForegroundColor Green;
+    Node-Init;
   }
 } catch {
   if ($(Verify-Bin "nodist") -eq $False) {
     Write-Host "nodist missing. Try https://github.com/marcelklehr/nodist/releases/latest" -ForegroundColor Yellow;
   } else {
-    Write-Host "nodist $(nodist --version)" -ForegroundColor Green;
-    try {
-      Write-Host "node $(node --version)" -ForegroundColor Green;
-    } catch {
-      Write-Host "node missing. Attempting to install..." -ForegroundColor Yellow;
-      nodist latest;
-      Write-Host "node $(node --version)" -ForegroundColor Green;
-    }
+    Node-Init;
   }
 }
