363f114e1cd9bb2abbdd4f434ddbc59da46e3540
Duy K. Bui <duy@buifamily.info>
2016-08-11 21:20:36 -0400

    No noise in e command

--- WindowsPowerShell/Scripts/e.ps1
+++ WindowsPowerShell/Scripts/e.ps1
@@ -1,7 +1,7 @@
 $code = Test-Path "$env:programfiles (x86)/Microsoft VS Code*/code*.exe";
 if ($code -eq $true) {
   $path = Resolve-Path "$env:programfiles (x86)/Microsoft VS Code*/code*.exe";
-  . "$path" $args;
+  Start-Process $path -ArgumentList $args;
 } else {
   notepad $args;
 }
