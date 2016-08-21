c048c485293a4476f33e0af9cd2aa3bac23fe3de
Duy K. Bui <duy@buifamily.info>
2016-08-21 11:24:19 -0400

    Update banner / node-init

--- WindowsPowerShell/Scripts/Node-Init.ps1
+++ WindowsPowerShell/Scripts/Node-Init.ps1
@@ -1,15 +1,9 @@
 if (Verify-Command "nodist") {
-	Write-Host "nodist $(nodist --version)" -ForegroundColor Green;
-  if (Verify-Command "node") {
-    Write-Host "node $(node --version)" -ForegroundColor Green;
-  } else {
-    Write-Host "node missing. Attempting to install..." -ForegroundColor Yellow;
-    nodist latest;
-    Node-Init;
-  }
+  Write-Output "nodist $(nodist --version)";
+  Write-Output "node $(node --version)";
 } else {
   if ($(Verify-Bin "nodist") -eq $False) {
-    Write-Host "nodist missing. Try https://github.com/marcelklehr/nodist/releases/latest" -ForegroundColor Yellow;
+    Write-Error "nodist missing. Try https://github.com/marcelklehr/nodist/releases/latest";
   } else {
     $env:NODIST_PREFIX = Resolve-Path("~/Programs/nodist");
     Node-Init;
--- banner.md
+++ banner.md
@@ -3,12 +3,9 @@
 ## Linux
 
 ```fish
-fisher edc/bass
 bass source ~/.nvm/nvm.sh --no-use ';' nvm use iojs
 ```
 
 - [NVM](https://github.com/creationix/nvm)
 
 ## Windows
-
-- [Nodist](https://github.com/marcelklehr/nodist/releases/download/v0.8.2/NodistSetup-v0.8.2.exe)
