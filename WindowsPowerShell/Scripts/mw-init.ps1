d09c25bf5257cf7bb9ee538a38575c635968dffa
Duy Bui <duybui@amazon.com>
2016-12-15 15:40:16 -0500

    Better mw-init

--- WindowsPowerShell/Scripts/mw-init.ps1
+++ WindowsPowerShell/Scripts/mw-init.ps1
@@ -1,4 +1,4 @@
-param([switch] $verbose)
+param([switch] $verbose, [switch] $update)
 
 $oldverbose = $VerbosePreference
 if ($verbose) {
@@ -6,9 +6,14 @@ if ($verbose) {
 }
 
 if (Verify-Bin "midway" -eq $True) {
+  if ($update -eq $True) {
+    $bin = Resolve-Path "~/Programs/midway/bin";
+    Write-Verbose "Download latest mwinit to $bin";
+    curl https://s3.amazonaws.com/com.amazon.aws.midway.software/windows/mwinit.exe -o "$bin/mwinit.exe"
+  }
   $key = Resolve-Path "~/.ssh/primary.pem.pub";
   Write-Verbose "Init Midway for $key";
-  mwinit -k "$key";
+  mwinit -k "$key" -o;
   scp ~/.ssh/*-cert.pub um-cli:~/.ssh;
   scp -r ~/.midway um-cli:~;
   ssh um-cli 'chmod 600 ~/.ssh/*-cert.pub';
