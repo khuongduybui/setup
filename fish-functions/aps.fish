6cfabec04da9f7b60ef6db83981fe8c4d9fe9521
Duy K. Bui <duybui@amazon.com>
2017-01-01 06:29:17 +0000

    Add ssh-corp

--- fish-functions/aps.fish
+++ fish-functions/aps.fish
@@ -6,6 +6,10 @@ function aps
 		set -lx BIT_CURL (/apollo/bin/env -e envImprovement which kcurl)
 		set -lx BIT_LOVES_PYTHON yes
 		set -lx BIT_REVIEWER_GROUPS aws-security-tanc
-		/apollo/bin/env -e envImprovement /apollo/bin/env -e WDETools /apollo/bin/env -e SDETools /usr/bin/fish
+#		if set -q BRAZIL_CLI_BIN
+#			/apollo/bin/env -e envImprovement /apollo/bin/env -e WDETools /apollo/bin/env -e BrazilCLI /usr/bin/fish
+#		else
+			/apollo/bin/env -e envImprovement /apollo/bin/env -e WDETools /apollo/bin/env -e SDETools /usr/bin/fish
+#		end
 	end
 end
