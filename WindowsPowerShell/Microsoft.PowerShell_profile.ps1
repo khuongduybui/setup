4fd9a4bf7e151b50d6285001e3b4c114d8808a72
Duy Bui <duybui@amazon.com>
2017-01-10 11:45:51 -0500

    Alias ssh-corp for no proxy

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -16,6 +16,7 @@ function    bcopy           { cmd /c copy /b $args; }
 function    myip            { ipconfig | gawk "/(adapter|IPv4 Address)/"; }
 Set-Alias   code            c2c.ps1
 function    sshp            { ssh -o ProxyCommand=None $args; }
+function    ssh-corp        { ssh -o ProxyCommand=None $args; }
 
 #GIT Functions
 function    g               { git status $args; }
