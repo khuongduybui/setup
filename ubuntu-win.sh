3ef60f99d4d9c69d8c60f504ac6534b637b66e21
Duy K. Bui <duy@buifamily.info>
2017-02-28 10:58:04 -0500

    Better OneDrive locator

--- ubuntu-win.sh
+++ ubuntu-win.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 echo '#! /bin/sh' > ~/bin/rsync-ssh.sh
-echo 'rsync -r /mnt/c/Users/duybui/OneDrive/Essentials/dotfile.ssh/* ~/.ssh' >> ~/bin/rsync-ssh.sh
+echo 'test -d /mnt/c/Users/duybui/OneDrive && rsync -r /mnt/c/Users/duybui/OneDrive/Essentials/dotfile.ssh/* ~/.ssh' >> ~/bin/rsync-ssh.sh
+echo 'test -d /mnt/d/OneDrive && rsync -r /mnt/d/OneDrive/Essentials/dotfile.ssh/* ~/.ssh' >> ~/bin/rsync-ssh.sh
 echo 'chmod 600 ~/.ssh/*.pem' >> ~/bin/rsync-ssh.sh
 echo 'chmod 600 ~/.ssh/config' >> ~/bin/rsync-ssh.sh
 echo 'chmod 644 ~/.ssh/*.pub' >> ~/bin/rsync-ssh.sh
