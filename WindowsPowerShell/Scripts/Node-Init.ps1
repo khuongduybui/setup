40381e04e8a2c2dcc6fb4e11ee2dc5cf8bd42b41
Duy K. Bui <duy@buifamily.info>
2017-03-04 16:27:38 -0500

    show npm version in node-init

--- WindowsPowerShell/Scripts/Node-Init.ps1
+++ WindowsPowerShell/Scripts/Node-Init.ps1
@@ -4,6 +4,7 @@ if (Verify-Command "nodist") {
   [Environment]::SetEnvironmentVariable("NODIST_PREFIX", $env:NODIST_PREFIX, "User")
   Write-Output "nodist $(nodist --version)";
   Write-Output "node $(node --version)";
+  Write-Output "npm $(npm --version)";
 } else {
   if ($(Verify-Bin "nodist") -eq $False) {
     Write-Error "nodist missing. Try https://github.com/marcelklehr/nodist/releases/latest";
