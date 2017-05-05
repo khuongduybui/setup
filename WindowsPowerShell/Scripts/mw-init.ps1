7b8828fa166a598c366202a7bbd593c3080ad6b4
Duy Bui <duybui@amazon.com>
2017-05-05 11:38:38 -0400

    ClearHost during mw-init

--- WindowsPowerShell/Scripts/mw-init.ps1
+++ WindowsPowerShell/Scripts/mw-init.ps1
@@ -9,14 +9,17 @@ if (Verify-Bin "midway" -eq $True) {
   if ($update -eq $True) {
     $bin = Resolve-Path "~/Programs/midway/bin";
     Write-Verbose "Download latest mwinit to $bin";
-    curl https://s3.amazonaws.com/com.amazon.aws.midway.software/windows/mwinit.exe -o "$bin/mwinit.exe"
+    Invoke-WebRequest https://s3.amazonaws.com/com.amazon.aws.midway.software/windows/mwinit.exe -o "$bin/mwinit.exe"
   }
+  Clear-Host;
   $key = Resolve-Path "~/.ssh/primary.pem.pub";
   Write-Verbose "Init Midway for $key";
   mwinit -k "$key" -o;
-  scp ~/.ssh/*-cert.pub um-cli:~/.ssh;
-  scp -r ~/.midway um-cli:~;
-  ssh um-cli 'chmod 600 ~/.ssh/*-cert.pub';
+  Clear-Host;
+  ssh um-cli mwinit -o --no-update-check;
+  Clear-Host;
+  ssh um-cli kinit -f;
+  Clear-Host;
 }
 
 $VerbosePreference = $oldverbose
