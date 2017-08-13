350bd0b865f252bc372d168e9072297834fd708d
Duy K. Bui <duy@buifamily.info>
2017-08-13 15:27:27 -0400

    Add ubuntu-win-sniper

--- ubuntu-win.sh
+++ ubuntu-win.sh
@@ -6,3 +6,9 @@ echo 'test -d /mnt/d/OneDrive && rsync -r /mnt/d/OneDrive/Essentials/dotfile.ssh
 echo 'chmod 600 ~/.ssh/*.pem' >> ~/bin/rsync-ssh.sh
 echo 'chmod 600 ~/.ssh/config' >> ~/bin/rsync-ssh.sh
 echo 'chmod 644 ~/.ssh/*.pub' >> ~/bin/rsync-ssh.sh
+bash ~/bin/rsync-ssh.sh
+
+sudo apt install -y mc screenfetch
+sudo chmod 664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+
+test -d /mnt/c/Users/duybui/code/sniper && bash ~/setup/ubuntu-win-sniper.sh
