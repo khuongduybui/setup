f04881bc1cb77e1d80c2044294a458811be5d26e
Duy K. Bui <duy@buifamily.info>
2017-08-14 18:34:11 -0400

    Use abbr

--- config.fish
+++ config.fish
@@ -36,39 +36,40 @@ end
 cat ~/setup/banner.md
 
 ##Abbreviations
-set -g fish_user_abbreviations 'g=git status' $fish_user_abbreviations
-set -g fish_user_abbreviations 'b=bit status' $fish_user_abbreviations
-set -g fish_user_abbreviations 'gco=git checkout' $fish_user_abbreviations
-set -g fish_user_abbreviations 'bco=bit checkout' $fish_user_abbreviations
-set -g fish_user_abbreviations 'gb=git branch' $fish_user_abbreviations
-set -g fish_user_abbreviations 'gbd=git branch -d' $fish_user_abbreviations
-set -g fish_user_abbreviations 'gbD=git branch -D' $fish_user_abbreviations
-set -g fish_user_abbreviations 'gdf=git diff --ignore-space-change' $fish_user_abbreviations
-set -g fish_user_abbreviations 'bdf=bit diff --ignore-space-change' $fish_user_abbreviations
-set -g fish_user_abbreviations 's=settings' $fish_user_abbreviations
-set -g fish_user_abbreviations 'e=edit' $fish_user_abbreviations
-set -g fish_user_abbreviations 'se=sedit' $fish_user_abbreviations
-set -g fish_user_abbreviations 'v=view' $fish_user_abbreviations
-set -g fish_user_abbreviations 'c=code' $fish_user_abbreviations
-set -g fish_user_abbreviations 'd=dot' $fish_user_abbreviations
-set -g fish_user_abbreviations 'a=abbr' $fish_user_abbreviations
-set -g fish_user_abbreviations 'acc=accounts' $fish_user_abbreviations
-set -g fish_user_abbreviations 'apa=apr Activate' $fish_user_abbreviations
-set -g fish_user_abbreviations 'apd=apr Deactivate' $fish_user_abbreviations
-set -g fish_user_abbreviations 'activate=aph --status Active' $fish_user_abbreviations
-set -g fish_user_abbreviations 'server=ape /apollo/env/SDETools/bin/brazil-build server' $fish_user_abbreviations
-set -g fish_user_abbreviations 'pkg=ape /apollo/end/SDETools/bin/brazil-build apollo-pkg' $fish_user_abbreviations
-set -g fish_user_abbreviations 'esudo=sudo -E' $fish_user_abbreviations
-set -g fish_user_abbreviations 'eossh=ssh -i .ssh/id_eos_ec2 -l ec2-user' $fish_user_abbreviations
-set -g fish_user_abbreviations 'lsenv=l /apollo/env/' $fish_user_abbreviations
-set -g fish_user_abbreviations 'cra=cr --all' $fish_user_abbreviations
-set -g fish_user_abbreviations 'pickaxe=ssh -At duybui-um.aka.amazon.com ssh -t aws-pickaxe-iad-prod-1a-i-24be46da.us-east-1.amazon.com /apollo/env/AWSPickaxeEscritoire/bin/pickaxe' $fish_user_abbreviations
-set -g fish_user_abbreviations 'grep-runtime-log=grep (brazil-bootstrap)/var/output/logs/* -e' $fish_user_abbreviations
-set -g fish_user_abbreviations 'rebuild-coral=clear; begin; set -lx SERVICE (pwe); cd ../"$SERVICE"Model/; brazil-build; cd ../"$SERVICE"Generator/; brazil-build; cd ../"$SERVICE"; brazil-build server; end' $fish_user_abbreviations
-set -g fish_user_abbreviations 'ssh-corp=ssh -o ProxyCommand=None' $fish_user_abbreviations
-set -g fish_user_abbreviations 'awsp=aws --profile' $fish_user_abbreviations
-set -g fish_user_abbreviations 'byobu=byobu-launcher -S ~/byobu new-session -A -s default' $fish_user_abbreviations
-set -g fish_user_abbreviations 'update-git=git remote set-url origin git@bitbucket.org:khuongduybui/linux.git' $fish_user_abbreviations
-set -g fish_user_abbreviations 'reset-time=sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime' $fish_user_abbreviations
-set -g fish_user_abbreviations 'reset-fish=rm ~/.config/fish/fishd*' $fish_user_abbreviations
+abbr f fuck
+abbr reset-fish 'rm ~/.config/fish/fishd*'
+abbr reset-time 'sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime'
+abbr update-git 'git remote set-url origin git@bitbucket.org:khuongduybui/linux.git'
+abbr byobu 'byobu-launcher -S ~/byobu new-session -A -s default'
+abbr awsp 'aws --profile'
+abbr ssh-corp 'ssh -o ProxyCommand=None'
+abbr rebuild-coral 'clear; begin; set -lx SERVICE (pwe); cd ../"$SERVICE"Model/; brazil-build; cd ../"$SERVICE"Generator/; brazil-build; cd ../"$SERVICE"; brazil-build server; end'
+abbr grep-runtime-log 'grep (brazil-bootstrap)/var/output/logs/* -e'
+abbr pickaxe 'ssh -At duybui.aka.amazon.com ssh -t aws-pickaxe-iad-prod-1a-i-24be46da.us-east-1.amazon.com /apollo/env/AWSPickaxeEscritoire/bin/pickaxe'
+abbr cra 'cr --all'
+abbr lsenv 'l /apollo/env/'
+abbr esudo 'sudo -E'
+abbr pkg 'ape /apollo/end/SDETools/bin/brazil-build apollo-pkg'
+abbr server 'ape /apollo/env/SDETools/bin/brazil-build server'
+abbr activate 'aph --status Active'
+abbr apd 'apr Deactivate'
+abbr apa 'apr Activate'
+abbr acc accounts
+abbr a abbr
+abbr d dot
+abbr c code
+abbr v view
+abbr se sedit
+abbr e edit
+abbr s settings
+abbr bdf 'bit diff --ignore-space-change'
+abbr gdf 'git diff --ignore-space-change'
+abbr gbD 'git branch -D'
+abbr gbd 'git branch -d'
+abbr gb 'git branch'
+abbr bco 'bit checkout'
+abbr gco 'git checkout'
+abbr b 'bit status'
+abbr g 'git status'
+
 status --is-login; and status --is-interactive; and exec byobu-launcher -S ~/byobu new-session -A -s default
