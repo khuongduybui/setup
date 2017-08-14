d57d69fdeb0b46a69f8ec0d097baaacba593a380
Duy K. Bui <duy@buifamily.info>
2017-08-13 23:22:32 -0400

    Working font

--- config.fish
+++ config.fish
@@ -19,6 +19,8 @@ set -xg LANG en_US.UTF-8
 #set -g fish_color_status red
 #set -g -x fish_greeting ''
 set -g theme_color_scheme terminal2-dark-white
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
 
 ##Plugins
 #fisher 2>/dev/null
