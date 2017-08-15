1a9dda943f3eb98c9113281d5ea8d64236d0d3dd
Duy K. Bui <duy@buifamily.info>
2017-08-15 12:38:22 -0400

    Shorter date time

--- config.fish
+++ config.fish
@@ -18,6 +18,7 @@ set -xg LANG en_US.UTF-8
 #set -g fish_color_host magenta
 #set -g fish_color_status red
 #set -g -x fish_greeting ''
+set -g theme_date_format '+%Y-%m-%d %H:%M:%S %Z'
 set -g theme_color_scheme terminal2-dark-white
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
