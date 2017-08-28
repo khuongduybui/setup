2fcb9aea43c8bbba2dfff71c716de6f802e5c7cf
Duy K. Bui <duy.bui@lunarline.com>
2017-08-27 22:32:13 -0400

    Better pfw

--- fish-functions/pfw.fish
+++ fish-functions/pfw.fish
@@ -1,11 +1,13 @@
+# Defined in /tmp/fish.rjxNk2/pfw.fish @ line 2
 function pfw
-	if not which socat > /dev/null
-		sudo yum install -y socat
+	if not which socat >/dev/null
+		which yum >/dev/null; and sudo yum install -y socat
+		which apt >/dev/null; and sudo apt install -y socat
 	end
 	set destination 3000
 	if test (count $argv) = 2
 		set destination $argv[2]
 	end
 	echo "Forwarding port $argv[1] to $destination"
-	socat "tcp-listen:$argv[1],reuseaddr,fork" "tcp:localhost:$destination"
+	sudo socat "tcp-listen:$argv[1],reuseaddr,fork" "tcp:localhost:$destination"
 end
