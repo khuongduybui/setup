eeebe2d471e0ad84d0356fe99f7875e7aa776ca0
Bui <duybui@amazon.com>
2017-09-15 13:48:10 -0400

    Update syncroot finding

--- WindowsPowerShell/Scripts/find-onedrive.ps1
+++ WindowsPowerShell/Scripts/find-onedrive.ps1
@@ -1,11 +1,11 @@
 $OneDrive = (Get-ItemProperty -Path "hkcu:\Software\Microsoft\Windows\CurrentVersion\SkyDrive\" -Name UserFolder -ErrorAction Silent).UserFolder;
-if ($null -eq $OneDrive) {
+if ($Null -eq $OneDrive -Or (Test-Path $OneDrive) -eq $False) {
   $OneDrive = (Get-ItemProperty -Path "hkcu:\Software\Microsoft\OneDrive\" -Name UserFolder -ErrorAction Silent).UserFolder;
 }
-if ($null -eq $OneDrive) {
+if ($Null -eq $OneDrive -Or (Test-Path $OneDrive) -eq $False) {
   $OneDrive = Resolve-Path -ErrorAction Silent "~/OneDrive";
 }
-if ($null -eq $OneDrive) {
+if ($Null -eq $OneDrive) {
   Write-Error "OneDrive not found.";
 } else {
   Write-Output $OneDrive;
--- WindowsPowerShell/Scripts/find-work-docs.ps1
+++ WindowsPowerShell/Scripts/find-work-docs.ps1
@@ -1,21 +1,26 @@
-if ((Test-Path "~/WorkDocs") -eq $True) {
-  $root = Resolve-Path "~/WorkDocs";
+if ((Test-Path "W:\My Documents") -eq $True) {
+  $root = "W:\My Documents"
   Write-Output $root;
 } else {
-  if ((Test-Path "$($SpecialFolders['LocalApplicationData'])/Amazon WorkDocs/SyncClient") -eq $True) {
-    $WorkDocsInstallation = Resolve-Path "$($SpecialFolders['LocalApplicationData'])/Amazon WorkDocs/SyncClient";
-    if ((Test-Path "$WorkDocsInstallation/service.log") -eq $True) {
-      $lastRootLogEntry = (Select-String -Path "$($SpecialFolders['LocalApplicationData'])/Amazon WorkDocs/SyncClient/*.log*" -Pattern "sync root: ");
-      if ($null -ne $lastRootLogEntry) {
-        $root = ($lastRootLogEntry[-1] -Split "sync root: ")[-1];
-        Write-Output $root;
+  if ((Test-Path "~/WorkDocs") -eq $True) {
+    $root = Resolve-Path "~/WorkDocs";
+    Write-Output $root;
+  } else {
+    if ((Test-Path "$($SpecialFolders['LocalApplicationData'])/Amazon WorkDocs/SyncClient") -eq $True) {
+      $WorkDocsInstallation = Resolve-Path "$($SpecialFolders['LocalApplicationData'])/Amazon WorkDocs/SyncClient";
+      if ((Test-Path "$WorkDocsInstallation/service.log") -eq $True) {
+        $lastRootLogEntry = (Select-String -Path "$($SpecialFolders['LocalApplicationData'])/Amazon WorkDocs/SyncClient/*.log*" -Pattern "sync root: ");
+        if ($null -ne $lastRootLogEntry) {
+          $root = ($lastRootLogEntry[-1] -Split "sync root: ")[-1];
+          Write-Output $root;
+        } else {
+          Write-Error "sync root not found.";
+        }
       } else {
-        Write-Error "sync root not found.";
+        Write-Error "service.log not found.";
       }
     } else {
-      Write-Error "service.log not found.";
+      Write-Error "WorkDocs not found.";
     }
-  } else {
-    Write-Error "WorkDocs not found.";
   }
 }
