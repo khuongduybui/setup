1c45473fdbedf983e5c16cac12704a4c68be523b
Duy K. Bui <duybui@amazon.com>
2017-10-06 10:18:36 -0500

    Better brazil / odin

--- fish-functions/br.fish
+++ fish-functions/br.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.lkk8sA/br.fish @ line 2
 function br
-	brazil-build & bin/runtime.sh
+	brazil-build; and test -f bin/runtime.sh; and bash bin/runtime.sh
 end
--- fish-functions/odin-get-private-key.fish
+++ fish-functions/odin-get-private-key.fish
@@ -1,7 +1,9 @@
+# Defined in /tmp/fish.GpRdMe/odin-get-private-key.fish @ line 2
 function odin-get-private-key
+	echo "Backing up existing file if any"
+	test -f ~/.ssh/$argv[1].pem.bak; and rm ~/.ssh/$argv[1].pem.bak
+	test -f ~/.ssh/$argv[1].pem; and mv ~/.ssh/$argv[1].pem ~/.ssh/$argv[1].pem.bak
 	echo "Fetching $argv[1] as ~/.ssh/$argv[1].pem"
-	rm ~/.ssh/$argv[1].pem.bak
-	mv ~/.ssh/$argv[1].pem ~/.ssh/$argv[1].pem.bak
 	echo "-----BEGIN RSA PRIVATE KEY-----" > ~/.ssh/$argv[1].pem
 	/apollo/env/envImprovement/bin/odin-get -t PrivateKey $argv[1] | base64 >> ~/.ssh/$argv[1].pem
 	echo "-----END RSA PRIVATE KEY-----" >> ~/.ssh/$argv[1].pem
