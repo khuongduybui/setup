37904df9e0670dd4a8ce1a513f013c7d67735b48
Duy K. Bui <duy@buifamily.info>
2017-10-29 10:28:56 -0400

    No need for GUI at the time

--- ubuntu-win-sniper.sh
+++ ubuntu-win-sniper.sh
@@ -3,7 +3,7 @@ sudo apt install -y curl git software-properties-common dirmngr python build-ess
 sudo npm install -g yarn forever npm-check-updates
 
 sudo service dbus start
-sudo apt install -y lubuntu-core
+# sudo apt install -y lubuntu-core
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
