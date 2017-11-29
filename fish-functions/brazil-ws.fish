6373f4316b7fdce0355aadc4878eb0cd9bc78467
Duy K. Bui <duybui@amazon.com>
2017-11-29 11:23:24 -0500

    Better brazil-ws

--- fish-functions/brazil-ws.fish
+++ fish-functions/brazil-ws.fish
@@ -1,12 +1,11 @@
-# Defined in /tmp/fish.OnfDFV/brazil-ws.fish @ line 2
+# Defined in /tmp/fish.BAR24h/brazil-ws.fish @ line 2
 function brazil-ws
-	echo $argv[1]
 	cd ~/code
-	$BRAZIL_CLI_BIN/brazil ws create --name=$argv[1]
+	eval "$BRAZIL_CLI_BIN/brazil" ws create --name=$argv[1]
 	cd $argv[1]
 	mkdir -p ~/env/$argv[1]
 	ln -s ~/env/$argv[1] env
 	mkdir -p ~/build/$argv[1]
 	ln -s ~/build/$argv[1] build
-	$BRAZIL_CLI_BIN/brazil setup platform-support
+	eval "$BRAZIL_CLI_BIN/brazil" setup platform-support
 end
--- ubuntu-amazon.sh
+++ ubuntu-amazon.sh
@@ -1,14 +1,11 @@
 # Amazon
 wget --no-check-certificate -qO - https://cascadia.corp.amazon.com/amazon/clienteng.gpg | sudo apt-key add -
-echo deb https://cascadia.corp.amazon.com/amazon xenial-amazon main | sudo tee /etc/apt/sources.list.d/amazon.list
-echo deb https://cascadia.corp.amazon.com/amazon xenial--thirdparty-partner partner | sudo tee -a /etc/apt/sources.list.d/amazon.list
+echo deb http://cascadia.corp.amazon.com/amazon xenial-amazon main | sudo tee /etc/apt/sources.list.d/amazon.list
+# echo deb http://cascadia.corp.amazon.com/amazon xenial--thirdparty-partner partner | sudo tee -a /etc/apt/sources.list.d/amazon.list
 sudo apt update
 sudo apt install -y openssh-server amazon-desktop-dhcp-config amazon-desktop-management amazon-firstboot2 amazon-firefoxconfig-dev amazon-pbis-config apt-transport-https
 
 # Latest GIT
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo apt update
-sudo install -y git
 test -f ~/.gitignore && mv ~/.gitignore ~/.gitignore.bak
 ln -s ~/setup/amazon.gitignore ~/.gitignore
 test -f ~/.gitconfig && mv ~/.gitconfig ~/.gitconfig.bak
