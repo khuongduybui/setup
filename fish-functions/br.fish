b1055713fbc885d53c5a138a737eb1643a2da03a
Duy K. Bui <duybui@amazon.com>
2017-12-01 23:02:00 -0500

    Better amazon stuff

--- fish-functions/br.fish
+++ fish-functions/br.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.lkk8sA/br.fish @ line 2
+# Defined in /tmp/fish.klk4fr/br.fish @ line 2
 function br
-	brazil-build; and test -f bin/runtime.sh; and bash bin/runtime.sh
+	brazil-build; and test -f bin/runtime.sh; and bash bin/runtime.sh $argv
 end
