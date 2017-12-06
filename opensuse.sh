c6e2ed9b78f4aa78b30bfb60b7c218e8490b3273
Duy K. Bui <duy@buifamily.info>
2017-12-06 00:02:17 -0500

    Better opensuse-win

--- opensuse.sh
+++ opensuse.sh
@@ -6,7 +6,7 @@ echo "Install system utils"
 sudo zypper addrepo http://download.opensuse.org/repositories/shells:fish:release:2/openSUSE_Leap_42.3/shells:fish:release:2.repo
 sudo zypper addrepo http://download.opensuse.org/repositories/utilities/openSUSE_Leap_42.3/utilities.repo
 sudo zypper refresh
-sudo zypper install -y fish vim neovim byobu
+sudo zypper install -y fish vim neovim tmux byobu
 sudo zypper install -y mc screenfetch
 
 echo "Install dev tools"
