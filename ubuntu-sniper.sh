80fb76acbf3082b8419e327a00452b589d149554
Duy K. Bui <duy@buifamily.info>
2017-12-09 13:34:09 -0500

    Ubuntu Sniper generic

--- ubuntu-sniper.sh
+++ ubuntu-sniper.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 sudo apt update
 sudo apt install -y curl git software-properties-common dirmngr python build-essential
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -12,7 +14,7 @@ sudo service dbus start
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt update
-sudo apt install -y mongodb
+test -d /mnt/c/Users/duybui && sudo apt install -y mongodb
 sudo apt install -y mongodb-org
 
 sudo apt install -y libfontconfig1 aha
