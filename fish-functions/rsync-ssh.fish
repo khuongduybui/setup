3f39c937cd9770fc6195f820b4603d5450d252ff
Duy K. Bui <duy@buifamily.info>
2017-12-09 21:46:49 -0500

    Move rsync-ssh into a fish function

--- fish-functions/rsync-ssh.fish
+++ fish-functions/rsync-ssh.fish
@@ -1,5 +1,6 @@
-# Defined in /tmp/fish.D0FnbC/rsync-ssh.fish @ line 1
+# Defined in /tmp/fish.YZy348/rsync-ssh.fish @ line 2
 function rsync-ssh
+	mkdir -p ~/.ssh
 	test -d /mnt/c/Users/duybui/OneDrive; and rsync -r /mnt/c/Users/duybui/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
 	test -d /mnt/d/OneDrive; and rsync -r /mnt/d/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
 	chmod 600 ~/.ssh/*.pem
