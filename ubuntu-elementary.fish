664cc5cde65957148d474d704d475d2efabe75b5
Duy K. Bui <duy@buifamily.info>
2017-12-14 01:35:16 -0500

    Better ubuntu-elementary

--- ubuntu-elementary.fish
+++ ubuntu-elementary.fish
@@ -11,5 +11,9 @@ sudo apt dist-upgrade -y
 
 sudo mkdir -p /etc/io.elementary.appcenter
 sudo touch /etc/io.elementary.appcenter/appcenter.blacklist
+
 gconftool --set --type=string /orgs/pantheon/-terminal/settings/encoding UTF-8
+
+mkdir -p ~/.config
+ln -s ~/setup/gtk-3.0 ~/.config
 
