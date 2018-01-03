5ad8829bb12f660ff16860054aa03242ead09b66
Duy K. Bui <duy@buifamily.info>
2018-01-02 20:03:32 -0500

    pretty bit

--- fish-functions/execute_bit.fish
+++ fish-functions/execute_bit.fish
@@ -1,16 +1,18 @@
-# Defined in /tmp/fish.jJ9kIA/execute_bit.fish @ line 1
 function execute_bit
-	set_color -o $fish_color_user
-	echo -n (basename (pwd))
-	set_color normal
-	set_color $fish_color_quote
-	echo -n (__fish_git_prompt)
-	set_color normal
-	echo -n '> git '
-	set_color -o $fish_color_host
-	echo -n $argv
-	set_color normal
-	echo
-	git $argv
-	echo
+    set_color -b blue white
+    echo -n ' '(basename (pwd))' '
+    set_color -b magenta blue
+    echo -n \uE0B0
+    set_color -b magenta white
+    echo -n (__fish_git_prompt)' '
+    set_color -b normal magenta
+    echo -n \uE0B0
+    set_color normal
+    echo -n ' 'git' '
+    set_color cyan
+    echo -n $argv
+    set_color normal
+    echo
+    git $argv
+    echo
 end
