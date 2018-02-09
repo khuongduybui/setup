0a918ad720d936996ea1bcf590db88537bd0607e
Duy K. Bui <duy@buifamily.info>
2018-02-09 00:17:42 -0500

    cp wsl.conf

--- ubuntu-win.sh
+++ ubuntu-win.sh
@@ -2,4 +2,4 @@
 
 test -f /etc/wsl.conf.bak && sudo rm /etc/wsl.conf.bak
 test -f /etc/wsl.conf && sudo mv /etc/wsl.conf /etc/wsl.conf.bak
-sudo ln -s ~/setup/wsl.conf /etc
+sudo cp -s ~/setup/wsl.conf /etc
