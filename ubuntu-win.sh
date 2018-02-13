3ecf9d41a6f0585128da1b896f965a75f751987e
Duy K. Bui <duy@buifamily.info>
2018-02-12 21:55:05 -0500

    cp no symlink wsl.conf

--- ubuntu-win.sh
+++ ubuntu-win.sh
@@ -2,4 +2,4 @@
 
 test -f /etc/wsl.conf.bak && sudo rm /etc/wsl.conf.bak
 test -f /etc/wsl.conf && sudo mv /etc/wsl.conf /etc/wsl.conf.bak
-sudo cp -s ~/setup/wsl.conf /etc
+sudo cp ~/setup/wsl.conf /etc
