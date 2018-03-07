93925ebd18580b2a554597ea666464c9777d78e0
Duy K. Bui <duybui@amazon.com>
2018-03-06 21:02:13 -0500

    Better choco installation

--- WindowsPowerShell/Scripts/choco-init.ps1
+++ WindowsPowerShell/Scripts/choco-init.ps1
@@ -15,7 +15,7 @@ if ((Verify-Command "choco") -eq $true) {
     Write-Host "choco missing! Run Choco-Init -f to install." -ForegroundColor Red;
   } else {
     Write-Host "choco missing! Fetching..." -ForegroundColor Yellow;
-    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'));
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
     return Choco-Init $f $l $u;
   }
 }
