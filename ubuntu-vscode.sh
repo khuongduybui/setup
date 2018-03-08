56d9a04ae12f3b3390d0015492185cc12c823e22
Duy K. Bui <duy@buifamily.info>
2018-03-07 23:20:43 -0500

    Minor update

--- ubuntu-vscode.sh
+++ ubuntu-vscode.sh
@@ -4,5 +4,5 @@ wget --no-check-certificate -qO - https://packages.microsoft.com/keys/microsoft.
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 # sudo add-apt-repository -y ppa:therealkenc/wsl-pulseaudio
 sudo apt update -y
-sudo apt upgrade -y
+# sudo apt upgrade -y
 sudo apt install -y code
