683a1e4c17b823018334c98699e568e4757cb929
Duy K. Bui <duy@buifamily.info>
2018-03-21 21:23:16 -0400

    Minor update to x-init

--- ubuntu-vscode.sh
+++ ubuntu-vscode.sh
@@ -4,4 +4,4 @@ wget --no-check-certificate -qO - https://packages.microsoft.com/keys/microsoft.
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo add-apt-repository -y ppa:therealkenc/wsl-pulseaudio
 sudo apt update -y
-sudo apt install -y code pulseaudio=1:8.0-0ubuntu3.7ppa1 libpulse0=1:8.0-0ubuntu3.7ppa1 libpulse-mainloop-glib0=1:8.0-0ubuntu3.7ppa1 pulseaudio-utils=1:8.0-0ubuntu3.7ppa1 libpulsedsp=1:8.0-0ubuntu3.7ppa1 pulseaudio-module-x11=1:8.0-0ubuntu3.7ppa1
+sudo apt install -y --allow-downgrades code pulseaudio=1:8.0-0ubuntu3.7ppa1 libpulse0=1:8.0-0ubuntu3.7ppa1 libpulse-mainloop-glib0=1:8.0-0ubuntu3.7ppa1 pulseaudio-utils=1:8.0-0ubuntu3.7ppa1 libpulsedsp=1:8.0-0ubuntu3.7ppa1 pulseaudio-module-x11=1:8.0-0ubuntu3.7ppa1
