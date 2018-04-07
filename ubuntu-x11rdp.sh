cd528f2c86fcb270eb2d272dd9b5540136adf008
Duy K. Bui <duy@buifamily.info>
2018-04-07 11:34:08 -0400

    Add xrdp.ini

--- ubuntu-x11rdp.sh
+++ ubuntu-x11rdp.sh
@@ -1,8 +1,9 @@
-!#/bin/sh
+#!/bin/sh
 
 sudo apt install -y automake autoconf build-essential pkgconf zlib1g-dev
 sudo mkdir -p /opt/X11rdp
-cd /opt/X11rdp/
+sudo chown $USER /opt/X11rdp
+cd /opt/X11rdp
 wget http://server1.xrdp.org/xrdp/x11rdp_xorg71.tar.gz
 tar -xvf x11rdp_xorg71.tar.gz
 cd x11rdp_xorg71
