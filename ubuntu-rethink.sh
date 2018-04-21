be85aab3900c51841e042f057315935fab411268
Duy K. Bui <duy@buifamily.info>
2018-04-21 18:11:39 -0400

    Add rethinkdb service

--- ubuntu-rethink.sh
+++ ubuntu-rethink.sh
@@ -4,3 +4,4 @@ source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt update -y
 sudo apt install -y rethinkdb
+sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf
