6f9c11cbcaf5ed15da195a60e856f2f1d68a5b41
Duy K. Bui <duybui@amazon.com>
2018-05-09 12:30:25 -0400

    Better aps

--- fish-functions/aps.fish
+++ fish-functions/aps.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/dz/y2q7pw153pz7gm62rj5n25_9qzr_wy/T//fish.aDBVvX/aps.fish @ line 2
+# Defined in /tmp/fish.pisri7/aps.fish @ line 2
 function aps
 	set -lx ADDITIONAL_PATH $PATH
 	test -d /apollo/env/envImprovement; and set -lx ADDITIONAL_PATH $ADDITIONAL_PATH /apollo/env/envImprovement/bin
@@ -11,20 +11,14 @@ function aps
 	test -d /apollo/env/NodeJS; and set -lx ADDITIONAL_PATH $ADDITIONAL_PATH /apollo/env/NodeJS/bin
 	test -d /apollo/env/Sigv4Curl; and set -lx ADDITIONAL_PATH $ADDITIONAL_PATH /apollo/env/Sigv4Curl/bin
 
-	if not test (count $argv) = 0
-		set -q BRAZIL_CLI_BIN; or set -lx BRAZIL_CLI_BIN ~/.toolbox/bin
-		set -lx PATH $ADDITIONAL_PATH $BRAZIL_CLI_BIN
-		if __is_mac
-			/usr/local/bin/fish
-		else
-			/usr/bin/fish
-		end
+	set -q BRAZIL_CLI_BIN; or set -lx BRAZIL_CLI_BIN ~/.toolbox/bin
+    set -lx SDE_CLI_BIN /apollo/env/SDETools/bin
+
+    set -lx PATH $ADDITIONAL_PATH $BRAZIL_CLI_BIN
+    test (count $argv) = 0; and test -x $SDE_CLI_BIN/brazil; and set -lx PATH $ADDITIONAL_PATH $SDE_CLI_BIN
+	if __is_mac
+		/usr/local/bin/fish
 	else
-		set -lx PATH $ADDITIONAL_PATH /apollo/env/SDETools/bin
-		if __is_mac
-			/usr/local/bin/fish
-		else
-			/usr/bin/fish
-		end
+		/usr/bin/fish
 	end
 end
