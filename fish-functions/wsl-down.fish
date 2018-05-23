e28ab6c6604b41b28e8892f650f15653f0f200ab
Duy K. Bui <duybui@amazon.com>
2018-05-23 12:02:36 -0400

    Better wsl-down

--- fish-functions/wsl-down.fish
+++ fish-functions/wsl-down.fish
@@ -1,10 +1,19 @@
-# Defined in /tmp/fish.R4A2TA/wsl-down.fish @ line 2
+# Defined in /tmp/fish.M4dDKo/wsl-down.fish @ line 2
 function wsl-down
 	echo 'Attempting to shutdown this WSL instance.'
 	if __read_confirm
 		sudo service mongodb stop
 		sudo service rethinkdb stop
+
 		sudo service dbus stop
+
+        if test -f /home/duybui/brazil-pkg-cache/daemon-pid
+            set -l pkgcache (string split : (cat /home/duybui/brazil-pkg-cache))[1]
+		    if test (ps aux | grep $pkgcache | grep -v grep | wc -l) -gt 0
+		        sudo kill -9 $pkgcache
+		    end
+		end
+
 		exit
 	end
 end
