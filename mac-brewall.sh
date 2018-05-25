9ea4d5795d2471667a8a69333dae364dc1e4c90c
Duy K. Bui <duy@buifamily.info>
2018-05-25 10:32:02 -0400

    Add mac-brewall.sh

---
+++ mac-brewall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+brew update; brew upgrade; brew cleanup
+updated=`brew cask outdated --quiet`
+if [ $updated ]; then brew cask reinstall $updated; fi
+echo "Done. HomeBrew content is up to date."
