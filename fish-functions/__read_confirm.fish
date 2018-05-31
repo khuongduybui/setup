80a4e104ec43fc739a79f5235a61224f83076885
Duy K. Bui <duy@buifamily.info>
2018-05-30 23:59:02 -0400

    Use get and getops in __read_confirm

--- fish-functions/__read_confirm.fish
+++ fish-functions/__read_confirm.fish
@@ -1,13 +1,18 @@
-# Defined in /tmp/fish.BwnNgd/__read_confirm.fish @ line 1
+# Defined in /tmp/fish.RYYt16/__read_confirm.fish @ line 2
 function __read_confirm
-	while true
-		read -l -P 'Do you want to continue? [y/N] ' confirm
+	set -l default n
 
-		switch $confirm
-			case Y y
-				return 0
-			case '' N n
-				return 1
+	getopts $argv | while read -l key value
+		switch $key
+			case default
+				set default $value
 		end
+	end
+
+	switch (get --prompt="Do you want to continute? [y/n, default: $default]" --default=$default --rule=[yn] --no-case --silent=1)
+		case y
+			return 0
+		case n
+			return 1
 	end
 end
