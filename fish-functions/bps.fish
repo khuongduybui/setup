166656f7e9868e14d36f9cd0cb264b49c55192d4
Duy K. Bui <duybui@amazon.com>
2018-06-05 18:01:51 -0400

    Add bps profile

--- fish-functions/bps.fish
+++ fish-functions/bps.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.ZkeYx1/bps.fish @ line 2
+# Defined in /tmp/fish.5vuy34/bps.fish @ line 2
 function bps
 	set -lx BARK_ACCOUNT
     set -lx BARK_MS
@@ -12,14 +12,26 @@ function bps
                 set -x BARK_MS $value
             case region
                 set -x BARK_REGION $value
+            case profile
+                set -x BARK_PROFILE $value
+                if not test -f ~/.config/bark/profiles/$value
+                    echo Profile $value not found under ~/.config/bark/profiles
+                    return -1
+                end
         end
     end
 
-    abbr bark 'bark -a='$BARK_ACCOUNT' -ms='$BARK_MS' -ep=https://timber-corp-'$BARK_REGION'.'$BARK_REGION'.proxy.amazon.com'
+    if test -n "$BARK_PROFILE"
+        abbr bark 'bark -cf=~/.config/bark/profiles/'$BARK_PROFILE
+    else
+        abbr bark 'bark -a='$BARK_ACCOUNT' -ms='$BARK_MS' -ep=https://timber-corp-'$BARK_REGION'.'$BARK_REGION'.proxy.amazon.com'
+    end
+    
     if __is_mac
         /usr/local/bin/fish
     else
         /usr/bin/fish
     end
+
     abbr -e bark
 end
