ccf1c628efc743c352656961380957daa71bdc41
Duy K. Bui <duybui@amazon.com>
2018-06-07 14:56:12 -0400

    WorkDocs Drive no longer holds Essentials

--- fish-functions/rsync-aws.fish
+++ fish-functions/rsync-aws.fish
@@ -1,8 +1,8 @@
-# Defined in /var/folders/dz/y2q7pw153pz7gm62rj5n25_9qzr_wy/T//fish.VojYjK/rsync-aws.fish @ line 2
+# Defined in /tmp/fish.wdB9MF/rsync-aws.fish @ line 2
 function rsync-aws
 	mkdir -p ~/.aws
 	test -d ~/OneDrive; and rsync -r ~/OneDrive/Essentials/dotfile.aws/* ~/.aws
 	test -d $WHOME/OneDrive; and rsync -r $WHOME/OneDrive/Essentials/dotfile.aws/* ~/.aws
 	test -d /mnt/d/OneDrive; and rsync -r /mnt/d/OneDrive/Essentials/dotfile.aws/* ~/.aws
-	test -d /mnt/w/My\ Documents; and rsync -r /mnt/w/My\ Documents/Essentials/dotfile.aws/* ~/.aws
+	# test -d /mnt/w/My\ Documents; and rsync -r /mnt/w/My\ Documents/Essentials/dotfile.aws/* ~/.aws
 end
--- fish-functions/rsync-ssh.fish
+++ fish-functions/rsync-ssh.fish
@@ -1,10 +1,10 @@
-# Defined in /var/folders/dz/y2q7pw153pz7gm62rj5n25_9qzr_wy/T//fish.Wfy1o7/rsync-ssh.fish @ line 2
+# Defined in /tmp/fish.Rnklzb/rsync-ssh.fish @ line 2
 function rsync-ssh
 	mkdir -p ~/.ssh
 	test -d ~/OneDrive; and rsync -r ~/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
 	test -d $WHOME/OneDrive; and rsync -r $WHOME/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
 	test -d /mnt/d/OneDrive; and rsync -r /mnt/d/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
-	test -d /mnt/w/My\ Documents; and rsync -r /mnt/w/My\ Documents/Essentials/dotfile.ssh/* ~/.ssh
+	# test -d /mnt/w/My\ Documents; and rsync -r /mnt/w/My\ Documents/Essentials/dotfile.ssh/* ~/.ssh
 	chmod 600 ~/.ssh/*.pem
 	chmod 600 ~/.ssh/config
 	chmod 644 ~/.ssh/*.pub
