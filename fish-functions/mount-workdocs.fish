ccc332f7ee63d2126603c455f96eacc81ba2e319
Duy K. Bui <duy@buifamily.info>
2018-06-25 00:10:34 -0400

    Suppress error from mount-workdocs

--- fish-functions/mount-workdocs.fish
+++ fish-functions/mount-workdocs.fish
@@ -1,5 +1,6 @@
-# Defined in /tmp/fish.Y7Fyvh/mount-workdocs.fish @ line 2
+# Defined in /tmp/fish.4AuBrI/mount-workdocs.fish @ line 2
 function mount-workdocs
-	sudo mkdir -p /mnt/w
-	sudo mount -t drvfs W: /mnt/w >/dev/null 2>&1; or true
+	sudo mkdir -p /mnt/w 2>/dev/null;
+	and sudo mount -t drvfs W: /mnt/w >/dev/null 2>&1;
+	or true
 end
