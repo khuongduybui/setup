81394edfa3d53c1689905ef9df5418aa05876fdc
Duy K. Bui <duy@buifamily.info>
2018-06-29 02:11:38 -0400

    Add inotify mod to al2-vscode

--- al2-vscode.sh
+++ al2-vscode.sh
@@ -4,3 +4,7 @@
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo yum install code
+
+# https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+printf "\nfs.inotify.max_user_watches = 1000000\n" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
