c3a274c47ed755177fc77506d6ea078d8f8e556e
Duy K. Bui <duy@buifamily.info>
2018-06-29 02:03:21 -0400

    Install Visual Studio Code on Amazon Linux 2

--- al2-sniper.sh
+++ al2-sniper.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
 sudo yum install -y curl
+# https://yarnpkg.com/lang/en/docs/install/#centos-stable
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo yum install -y yarn
 yarn global add forever npm-check-updates electron-forge nodemon aurelia-cli @feathersjs/cli
 
+# https://docs.mongodb.com/tutorials/install-mongodb-on-amazon/
 sudo cp ~/setup/mongodb-org-3.6.repo /etc/yum.repos.d
 sudo yum install -y mongodb-org
 
