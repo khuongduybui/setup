db33461a255a4ea42993e1042b7f7ea982e88c8f
Duy K. Bui <duy@buifamily.info>
2018-07-10 16:44:32 -0400

    Add external IP to myip

--- fish-functions/myip.fish
+++ fish-functions/myip.fish
@@ -1,4 +1,7 @@
+# Defined in /tmp/fish.fZUToY/myip.fish @ line 2
 function myip
+	which curl >/dev/null 2>&1; and segment blue white " External IP "; and segment_close; and curl https://www.myexternalip.com/raw
+	segment blue white " Internal IP "; segment_close; echo;
 	if __is_mac
 		ifconfig | fgrep inet | grep --color -E " ([0-9a-f]*[\.:]?)+[%a-z0-9]* "
 	else
