2f23ae9e17f4144f2eafd619ad411c7f6fc5d222
Duy K. Bui <duy@buifamily.info>
2018-08-03 15:01:00 -0400

    Color in myip

--- fish-functions/myip.fish
+++ fish-functions/myip.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.fZUToY/myip.fish @ line 2
+# Defined in /tmp/fish.nB8kGV/myip.fish @ line 2
 function myip
-	which curl >/dev/null 2>&1; and segment blue white " External IP "; and segment_close; and curl https://www.myexternalip.com/raw
+	which curl >/dev/null 2>&1; and segment blue white " External IP "; and segment_close; and curl -s https://www.myexternalip.com/raw | grep .
 	segment blue white " Internal IP "; segment_close; echo;
 	if __is_mac
 		ifconfig | fgrep inet | grep --color -E " ([0-9a-f]*[\.:]?)+[%a-z0-9]* "
