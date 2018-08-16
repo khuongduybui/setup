81bef0e2ba0a2932df8887ef81027c714e053513
Duy K. Bui <duy@buifamily.info>
2018-08-16 14:51:03 -0400

    Update rhel-amazon.sh

--- rhel-amazon.sh
+++ rhel-amazon.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# install unison
-unison_ver=2.48.3
-wget http://www.seas.upenn.edu/~bcpierce/unison//download/releases/unison-${unison_ver}/unison-${unison_ver}.tar.gz
-wget http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.3.tar.gz
-tar xvf unison-${unison_ver}.tar.gz
-tar xvf ocaml-4.02.3.tar.gz
-cd ocaml-4.02.3
-./configure
-gmake world && gmake opt && sudo gmake install
-cd ../unison-${unison_ver}
-gmake
-sudo mv ./unison /usr/bin/
-sudo chown root ./unison-fsmonitor
-sudo mv ./unison-fsmonitor /usr/bin
+# install unison (disabled in favor of ninja-dev-sync)
+# unison_ver=2.48.3
+# wget http://www.seas.upenn.edu/~bcpierce/unison//download/releases/unison-${unison_ver}/unison-${unison_ver}.tar.gz
+# wget http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.3.tar.gz
+# tar xvf unison-${unison_ver}.tar.gz
+# tar xvf ocaml-4.02.3.tar.gz
+# cd ocaml-4.02.3
+# ./configure
+# gmake world && gmake opt && sudo gmake install
+# cd ../unison-${unison_ver}
+# gmake
+# sudo mv ./unison /usr/bin/
+# sudo chown root ./unison-fsmonitor
+# sudo mv ./unison-fsmonitor /usr/bin
 
 # install XRDP
 sudo yum install -y xrdp x11rdp xorgxrdp tigervnc-server
