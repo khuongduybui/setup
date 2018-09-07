067f99cb1aa937fa6da8b8d3ffdff2e15d6f670e
Duy K. Bui <duybui@amazon.com>
2018-09-07 19:28:27 -0400

    Better turtle

--- fish-functions/turtle.fish
+++ fish-functions/turtle.fish
@@ -1,9 +1,9 @@
-# Defined in /tmp/fish.I3mO94/turtle.fish @ line 1
+# Defined in /tmp/fish.ME9z40/turtle.fish @ line 2
 function turtle
-	set -l PWD (pwd)
+	set -l CURWD (pwd)
   set -l PWE (pwe)
   code turtle
-  brazil-build release
-  env APOLLO_ACTUAL_ENVIRONMENT_ROOT=/apollo/env/SDETools sudo -E (brazil-path run.runtimefarm)/bin/python2.7 (brazil-path run.runtimefarm)/bin/turtle_register.py --config $PWD/configuration/etc/turtle-config $PWE test $USER
-  cd $PWD
+  # brazil-build release
+  env APOLLO_ACTUAL_ENVIRONMENT_ROOT=/apollo/env/SDETools sudo -E (brazil-path run.runtimefarm)/bin/python2.7 (brazil-path run.runtimefarm)/bin/turtle_register.py --config $CURWD/configuration/etc/turtle-config $PWE desktop $USER
+  cd $CURWD
 end
