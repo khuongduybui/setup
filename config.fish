411e805fedf4d9f2390cdfc628260b8efba25ad0
Duy K. Bui <duy@buifamily.info>
2018-09-17 22:17:55 -0400

    feat(fish): .<script> => yarn/npm run --silent <script>

--- config.fish
+++ config.fish
@@ -27,7 +27,7 @@ set -g theme_color_scheme terminal2-dark-white
 
 ## Plugins
 #fisher 2>/dev/null
-test (which npx); and source (npx --shell-auto-fallback | psub)
+test -f ~/setup/fallback.fish; and source ~/setup/fallback.fish
 
 ## Windows?
 if __is_win
