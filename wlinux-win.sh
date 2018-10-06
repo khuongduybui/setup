d0ac099fc7cdfd401cf537a9e045b924e7929b3f
Duy K. Bui <duy@buifamily.info>
2018-10-06 17:10:43 -0400

    fix(wlinux): Explicitly set wslview as default browser

--- wlinux-win.sh
+++ wlinux-win.sh
@@ -3,3 +3,5 @@
 test -f /etc/wsl.conf.bak && sudo rm /etc/wsl.conf.bak
 test -f /etc/wsl.conf && sudo mv /etc/wsl.conf /etc/wsl.conf.bak
 sudo cp ~/setup/wsl.conf /etc
+
+sudo update-alternatives --set x-www-browser /usr/bin/wslview
