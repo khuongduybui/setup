651b33d1e52b4d16b647e1d1d57adb8175274a92
Duy K. Bui <duy@buifamily.info>
2018-10-06 23:03:55 -0400

    fix(WLinux): Update to match latest installation script

--- wlinux-amazon.sh
+++ wlinux-amazon.sh
@@ -10,45 +10,46 @@ ln -s ~/setup/amazon.gitconfig ~/.gitconfig
 
 # Amazon
 wget --no-check-certificate -qO - https://cascadia.corp.amazon.com/amazon/clienteng.gpg | sudo apt-key add -
-echo deb http://cascadia.corp.amazon.com/amazon xenial-amazon main | sudo tee /etc/apt/sources.list.d/amazon.list
-echo deb http://cascadia.corp.amazon.com/amazon xenial-thirdparty-partner partner | sudo tee -a /etc/apt/sources.list.d/amazon.list
+echo deb http://cascadia.corp.amazon.com/amazon trusty-amazon main | sudo tee /etc/apt/sources.list.d/amazon.list
+echo deb http://cascadia.corp.amazon.com/amazon trusty-thirdparty-partner partner | sudo tee -a /etc/apt/sources.list.d/amazon.list
 sudo apt update
 sudo apt install -y amazon-desktop-management
 
-# Midway
-sudo apt install mwinit
-mkdir -p ~/bin
-curl https://s3.amazonaws.com/com.amazon.aws.midway.software/linux/mcurl.sh > ~/bin/mcurl
-chmod +x ~/bin/mcurl
-test -f ~/.ssh/id_rsa.pub || ssh-keygen
-mwinit -o
+# # Midway
+# sudo apt install mwinit
+# mkdir -p ~/bin
+# curl https://s3.amazonaws.com/com.amazon.aws.midway.software/linux/mcurl.sh > ~/bin/mcurl
+# chmod +x ~/bin/mcurl
+# test -f ~/.ssh/id_rsa.pub || ssh-keygen
+# mwinit -o
 
-# Kerberos
-sudo apt install -y krb5-user krb5-multidev libkrb5-dev
-scp duybui.aka.amazon.com:/etc/krb5.conf ~
-sudo cp ~/krb5.conf /etc/krb5.conf
-sudo chown root:root /etc/krb5.conf
-kinit -f
+# # Kerberos
+# sudo apt install -y krb5-user krb5-multidev libkrb5-dev
+# ssh-keygen -f "~/.ssh/known_hosts" -R duybui.aka.amazon.com
+# scp duybui.aka.amazon.com:/etc/krb5.conf ~
+# sudo cp ~/krb5.conf /etc/krb5.conf
+# sudo chown root:root /etc/krb5.conf
+# kinit -f
 
-# Toolbox
-curl --negotiate -fLSsu: 'https://drive.corp.amazon.com/view/BuilderToolbox/toolbox-install.sh' -o /tmp/toolbox-install.sh
-bash /tmp/toolbox-install.sh ubuntu
+# # Toolbox
+# curl --negotiate -fLSsu: 'https://drive.corp.amazon.com/view/BuilderToolbox/toolbox-install.sh' -o /tmp/toolbox-install.sh
+# bash /tmp/toolbox-install.sh ubuntu
 
-~/.toolbox/bin/toolbox install cr
-~/.toolbox/bin/toolbox install brazilcli
+# ~/.toolbox/bin/toolbox install cr
+# ~/.toolbox/bin/toolbox install brazilcli
 
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt update
-sudo apt install -y openjdk-8-jdk-headless
-~/.toolbox/bin/brazil setup --java
+# sudo add-apt-repository -y ppa:openjdk-r/ppa
+# sudo apt update
+# sudo apt install -y openjdk-8-jdk-headless
+# ~/.toolbox/bin/brazil setup --java
 
-sudo apt install -y ruby
-~/.toolbox/bin/brazil setup --ruby
+# ~/.toolbox/bin/brazil setup --ruby
+# ~/.toolbox/bin/brazil setup --python
 
-# Ninja Dev Sync
-curl --negotiate -fu: 'https://devcentral.amazon.com/ac/brazil/package-master/package/view/NinjaDevSync%3B2.1.0.0%3BRHEL5_64%3BDEV.STD.PTHREAD%3Bbin/ninja-dev-sync.linux64' -o ~/.toolbox/bin/ninja-dev-sync
-chmod 755 ~/.toolbox/bin/ninja-dev-sync
-ln -s ~/.toolbox/bin/ninja-dev-sync ~/.toolbox/bin/nds
-sudo apt install -y inotify-tools
-printf "\nfs.inotify.max_user_watches = 1000000\n" | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
+# # Ninja Dev Sync
+# curl --negotiate -fu: 'https://devcentral.amazon.com/ac/brazil/package-master/package/view/NinjaDevSync%3B2.1.0.0%3BRHEL5_64%3BDEV.STD.PTHREAD%3Bbin/ninja-dev-sync.linux64' -o ~/.toolbox/bin/ninja-dev-sync
+# chmod 755 ~/.toolbox/bin/ninja-dev-sync
+# ln -s ~/.toolbox/bin/ninja-dev-sync ~/.toolbox/bin/nds
+# sudo apt install -y inotify-tools
+# printf "\nfs.inotify.max_user_watches = 1000000\n" | sudo tee -a /etc/sysctl.conf
+# sudo sysctl -p
--- wlinux.sh
+++ wlinux.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 echo "Install package manager"
-sudo apt install -y apt-transport-https lsb-release curl
+# sudo apt install -y gdebi
 
 echo "Install system utils"
 sudo apt install -y byobu grc rsync
 sudo apt install -y mc
 
 echo "Install dev tools"
-sudo apt install -y awscli
+sudo apt install -y python-pip python3-pip awscli
 
 echo "Update system"
