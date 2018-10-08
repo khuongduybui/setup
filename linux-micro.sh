043d124b94363a2ca016c654d98d931f9d53e6fc
Duy K. Bui <duybui@amazon.com>
2018-10-07 23:59:01 -0400

    fix(linux): Update micro installation script

--- linux-micro.sh
+++ linux-micro.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/opt
-cd ~/opt
-rm -rf micro-*
+cd /tmp
 wget https://github.com/zyedidia/micro/releases/download/v1.4.1/micro-1.4.1-linux64.tar.gz
 tar -zxf micro-*.tar.gz
 rm -f ~/bin/micro
-ln -s ~/opt/micro-*/micro ~/bin
-rm -f *.tar.gz
+cp /tmp/micro-*/micro ~/bin
+rm -rf micro-*
 cd -
