b69e750a02ef18073190f6d49149523a55794815
Duy K. Bui <duybui@amazon.com>
2018-10-07 22:24:18 -0400

    fix(fish): Turn down verbosity in varclear

--- fish-functions/varclear.fish
+++ fish-functions/varclear.fish
@@ -1,18 +1,12 @@
 function varclear --description 'Remove duplicates from environment variable'
-    if test (count $argv) = 1
+	if test (count $argv) = 1
         set -l newvar
-        set -l count 0
         for v in $$argv
-            if contains -- $v $newvar
-                set count (math $count+1)
-            else
+            if not contains -- $v $newvar
                 set newvar $newvar $v
             end
         end
         set $argv $newvar
-        test $count -gt 0
-        and echo Removed $count duplicates from $argv
-        or echo No duplicates found in $argv
     else
         for a in $argv
             varclear $a
