00a8fd12d18394e3ea39dcf63d5b2f21f7158e6c
Duy K. Bui <duybui@amazon.com>
2018-10-09 13:24:12 -0400

    fix(WLinux): Fix Brazil 2.0 installation (WIP)

--- wlinux-amazon.sh
+++ wlinux-amazon.sh
@@ -30,7 +30,16 @@ chmod +x ~/bin/mcurl
 test -f ~/.ssh/id_rsa.pub || ssh-keygen
 mwinit -o
 
-# sudo apt-get install git-core autoconf libtool gettext autopoint automake autogen nettle-dev libp11-kit-dev libtspi-dev guile-2.0-dev    libtasn1-6-dev libidn11-dev gawk gperf git2cl libunbound-dev bison help2man gtk-doc-tools texinfo texlive texlive-generic-recommended texlive-extra-utils librtmp-dev zlib1g-dev cmake libldap2-dev libgmp-dev pkg-config libudev-dev libusb-dev libgss3 libgssapi-krb5-2 libgss-dev libgnutls-dev libsasl2-dev libshishi-dev libgcrypt20-dev libdb5.3-dev libgpg-error-dev libsqlite3-dev libmysqlclient-dev libpq-dev libc-dev libfox-1.6-dev libcomerr2 make krb5-user libusb-1.0-0-dev g++
+# Kerberos
+sudo apt install -y krb5-user # krb5-multidev libkrb5-dev
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R duybui.aka.amazon.com
+scp duybui.aka.amazon.com:/etc/krb5.conf ~
+sudo cp ~/krb5.conf /etc/krb5.conf
+sudo chown root:root /etc/krb5.conf
+kinit -f
+# If you have problems with your Kerberos credentials syncing to your xrdp session:
+# Then add the following under the [libdefaults] section in /etc/krb5.conf:
+# default_ccache_name = /tmp/krb5cc_%{uid}
 
 # yubi_age=$(( $(date +"%s") - $(stat -c "%Y" ~/.ssh/id_rsa-cert.pub) ))
 # if [ "$yubi_age" -gt "36000" ] || ! klist -s; then
@@ -46,20 +55,13 @@ mwinit -o
 #     env -u PASSWD > /dev/null
 # fi
 
-# Kerberos
-sudo apt install -y krb5-user # krb5-multidev libkrb5-dev
-ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R duybui.aka.amazon.com
-scp duybui.aka.amazon.com:/etc/krb5.conf ~
-sudo cp ~/krb5.conf /etc/krb5.conf
-sudo chown root:root /etc/krb5.conf
-kinit -f
-# If you have problems with your Kerberos credentials syncing to your xrdp session:
-# Then add the following under the [libdefaults] section in /etc/krb5.conf:
-# default_ccache_name = /tmp/krb5cc_%{uid}
-
 # Toolbox
+echo 'DISTRIB_ID=Ubuntu' | sudo tee /etc/lsb-release
+echo 'DISTRIB_RELEASE=14.04' | sudo tee -a /etc/lsb-release
+echo 'DISTRIB_CODENAME=trusty' | sudo tee -a /etc/lsb-release
+echo 'DISTRIB_DESCRIPTION="Ubuntu 14.04 LTS"' | sudo tee -a /etc/lsb-release
 curl --negotiate -fLSsu: 'https://drive.corp.amazon.com/view/BuilderToolbox/toolbox-install.sh' -o /tmp/toolbox-install.sh
-bash /tmp/toolbox-install.sh ubuntu
+bash /tmp/toolbox-install.sh
 
 # CR tool
 ~/.toolbox/bin/toolbox install cr
@@ -67,41 +69,13 @@ bash /tmp/toolbox-install.sh ubuntu
 # Brazil 2.0
 sudo apt install -y openjdk-8-jdk-headless
 ~/.toolbox/bin/toolbox install brazilcli
-# @TODO brazil 2.0 fails with
-# WARN[0000] Unable to determine Linux platform, defaulting to alinux
-# /apollo is empty
-
-# WORKAROUND:
-# mv ~/.toolbox ~/.toolbox.bak
-# cp ~/.toolbox & /apollo from Ubuntu over
 # curl http://http.us.debian.org/debian/pool/main/r/readline6/libreadline6_6.3-8+b3_amd64.deb -o /tmp/libreadline6.deb
 # curl http://http.us.debian.org/debian/pool/main/r/readline6/libreadline6-dev_6.3-8+b3_amd64.deb -o /tmp/libreadline6-dev.deb
 # sudo apt install -y /tmp/libreadline6.deb /tmp/libreadline6-dev.deb
-# edit /home/duybui/.toolbox/tools/brazilcli/2.0.200620.0/ruby2.1.x/lib/ruby/site_ruby/2.1.0/amazon/brazil/any_os/linux.rb
-# 111   #
-# 112   # Assume Ubuntu (at this time)
-# 113   #
-# 114   def self.lsb_release
-# 115     path = '/etc/lsb-release'
-# 116     path2 = '/etc/os-release'
-# 117     return nil unless File.exist?(path) or File.exists?(path2)
-# 118     if File.exist?(path)
-# 119       if File.open(path, 'rb'){ |f| f.read } =~ /^DISTRIB_ID=Ubuntu/
-# 120         return "UBUNTU"
-# 121       end
-# 122     end
-# 123     if File.exist?(path2)
-# 124       if File.open(path2, 'rb'){ |f| f.read } =~ /WLinux/
-# 125         return "UBUNTU"
-# 126       end
-# 127     end
-# 128     return nil
-# 129     raise "Unrecognized LSB release"
-# 130   end
 
-# ~/.toolbox/bin/brazil setup --java
-# ~/.toolbox/bin/brazil setup --ruby
-# ~/.toolbox/bin/brazil setup --python
+~/.toolbox/bin/brazil setup --java
+~/.toolbox/bin/brazil setup --ruby
+~/.toolbox/bin/brazil setup --python
 
 # Ninja Dev Sync
 curl --negotiate -fu: 'https://devcentral.amazon.com/ac/brazil/package-master/package/view/NinjaDevSync%3B2.1.0.0%3BRHEL5_64%3BDEV.STD.PTHREAD%3Bbin/ninja-dev-sync.linux64' -o ~/.toolbox/bin/ninja-dev-sync
