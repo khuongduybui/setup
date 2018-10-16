b8dfffd4c3550d47c491c912e9cbfa24548fef2e
Duy K. Bui <duy@buifamily.info>
2018-10-15 20:23:15 -0400

    chore(arch): Minor fixes

--- arch.sh
+++ arch.sh
@@ -10,14 +10,15 @@ echo "Install package manager"
 # fakeroot is not working correctly due to Sys IPC not implemented
 
 echo "Install system utils"
-sudo pacman -S --noconfirm fish neovim xsel xclip grc mc screenfetch scrot rsync
+sudo pacman -Sy --noconfirm rsync openssh xorg-xauth x11-ssh-askpass
+sudo pacman -Sy --noconfirm fish neovim xsel xclip grc mc screenfetch scrot
 # @TODO install byobu
 
 echo "Install dev tools"
-sudo pacman -S --noconfirm nodejs yarn
-sudo pacman -S --noconfirm python2 python2-setuptools python2-pip
-sudo pacman -S --noconfirm python python-setuptools python-pip
-sudo pacman -S --noconfirm ruby
+sudo pacman -Sy --noconfirm nodejs yarn
+sudo pacman -Sy --noconfirm python2 python2-setuptools python2-pip
+sudo pacman -Sy --noconfirm python python-setuptools python-pip
+sudo pacman -Sy --noconfirm ruby
 
 echo "Update system"
 sudo pacman -Syu --noconfirm
