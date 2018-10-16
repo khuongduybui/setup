5fdbceebbde975fb3cd07246c7e493ae54ec4d23
Duy K. Bui <duy@buifamily.info>
2018-10-15 20:05:41 -0400

    feat(Arch): Add Arch Linux installation

--- arch.sh
+++ arch.sh
@@ -2,12 +2,22 @@
 
 echo "Install package manager"
 # @TODO install AUR
+# sudo pacman -S --noconfirm --needed base-devel
+# test -d /tmp/yay || git clone https://aur.archlinux.org/yay.git /tmp/yay
+# cd /tmp/yay
+# which yay >/dev/null || makepkg -si
+# cd -
+# fakeroot is not working correctly due to Sys IPC not implemented
 
 echo "Install system utils"
-# @TODO install fish, vim, neovim, byobu, grc, mc, screenfetch
+sudo pacman -S --noconfirm fish neovim xsel xclip grc mc screenfetch scrot
+# @TODO install byobu
 
 echo "Install dev tools"
-# @TODO install nodejs, yarn, python, pip, ruby, gem
+sudo pacman -S --noconfirm nodejs yarn
+sudo pacman -S --noconfirm python2 python2-setuptools python2-pip
+sudo pacman -S --noconfirm python python-setuptools python-pip
+sudo pacman -S --noconfirm ruby
 
 echo "Update system"
 sudo pacman -Syu --noconfirm
