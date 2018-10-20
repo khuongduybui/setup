35e9338e083c39d550ac2d68c5912d4b98a2c0da
Duy K. Bui <duy@buifamily.info>
2018-10-20 16:00:10 -0400

    chore: Re-group abbreviations

--- abbreviations.fish
+++ abbreviations.fish
@@ -1,32 +1,24 @@
 # Fish abbreviations, sourced in ~/.config/fish/config.fish
 
-abbr f 'fuck'
-abbr reset-fish 'rm ~/.config/fish/fishd*'
-abbr reset-time 'sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime'
-abbr update-git 'git remote set-url origin git@github.com:khuongduybui/setup.git'
+# abbr reset-fish 'rm ~/.config/fish/fishd*'
+# abbr reset-time 'sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime'
 # abbr byobu 'byobu-launcher -S ~/byobu new-session -A -s default'
 # abbr awsp 'aws --profile'
 abbr ssh-corp 'ssh -o ProxyCommand=None'
 abbr ssh-ec2 'ssh -i ~/.ssh/ec2.pem -l ec2-user'
 abbr ssh-odin 'ssh -N um-odin'
-abbr rebuild-coral 'clear; begin; set -lx SERVICE (pwe); cd ../"$SERVICE"Model/; brazil-build; cd ../"$SERVICE"Generator/; brazil-build; cd ../"$SERVICE"; brazil-build ser    ver; end'
-abbr grep-runtime-log 'grep (brazil-bootstrap)/var/output/logs/* -e'
-abbr pickaxe 'ssh -At duybui.aka.amazon.com ssh -t aws-pickaxe-iad-prod-1a-i-24be46da.us-east-1.amazon.com /apollo/env/AWSPickaxeEscritoire/bin/pickaxe'
-abbr cra 'cr --all'
-abbr lsenv 'l /apollo/env/'
-abbr esudo 'sudo -E'
-abbr pkg 'ape /apollo/end/SDETools/bin/brazil-build apollo-pkg'
-abbr server 'ape /apollo/env/SDETools/bin/brazil-build server'
-abbr activate 'aph --status Active'
-abbr apd 'apr Deactivate'
-abbr apa 'apr Activate'
+
+abbr update-git 'git remote set-url origin git@github.com:khuongduybui/setup.git'
+
 abbr acc 'accounts'
-abbr a 'add-abbr'
 abbr d 'dot'
 abbr c 'code'
 abbr v 'view'
 abbr e 'edit'
+abbr se 'sudo -E $EDITOR'
 abbr s 'settings'
+abbr cmd command
+
 abbr bdf 'bit diff --ignore-space-change'
 abbr gdf 'git diff --ignore-space-change'
 abbr bdfh 'bit diff --ignore-space-change HEAD'
@@ -39,18 +31,21 @@ abbr gco 'git checkout'
 abbr b 'bit status'
 abbr g 'git status'
 abbr y 'yarn'
-abbr se 'sudo -E $EDITOR'
 abbr gll 'git config --global user.email duy.bui@lunarline.com'
-abbr c9color 'set -g theme_color_scheme terminal2-dark-white'
+abbr gamz 'git config --global user.email duybui@amazon.com'
+
 abbr awscc 'git clone ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/'
-abbr nm 'ssh um-cli /apollo/bin/env -e envImprovement'
-abbr cmd command
 abbr region 'aws configure set region'
+abbr lsenv 'l /apollo/env/'
+
 abbr mongodb 'sudo service mongodb'
 abbr rethinkdb 'sudo service rethinkdb'
 abbr dbus 'sudo service dbus'
+
 abbr fisher-update 'fisher self-update; fisher; patch ~/.config/fish/functions/fish_prompt.fish ~/setup/aps.patch'
+
 abbr tpm 'npm install --save --package-lock-only'
 abbr tpd 'npm install --save-dev --only=dev'
+
 abbr wlinux-setup 'test -f /etc/setup; and bash /etc/setup'
 abbr wlinux-help 'test -f /etc/helpme; and bash /etc/helpme'
