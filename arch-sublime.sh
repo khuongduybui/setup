0bd714b6962b6b260a0ba0848b8920d1f7aa695c
Duy K. Bui <duy@buifamily.info>
2018-10-24 18:09:20 -0400

    fix(arch): No confirm when installing sublime

--- arch-sublime.sh
+++ arch-sublime.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-cd (mktemp --directory)    
+cd $(mktemp --directory)
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg
 sudo pacman-key --add sublimehq-pub.gpg
 sudo pacman-key --lsign-key 8A8F901A
 grep -q download.sublimetext.com /etc/pacman.conf || echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-sudo pacman -Syu --needed sublime-text sublime-merge
+sudo pacman -Syu --needed --noconfirm sublime-text sublime-merge
