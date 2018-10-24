72a25592031d5c8adc7e4441bd3536020a281f9f
Duy K. Bui <duybui@amazon.com>
2018-10-24 12:46:31 -0400

    fix(linux): launch X410 only when not running

--- fish-functions/x-init.fish
+++ fish-functions/x-init.fish
@@ -1,8 +1,12 @@
 function x-init
 	set_color $fish_color_operator; echo Launching X-Server; set_color normal
-	if test -x $W/AppData/Local/Microsoft/WindowsApps/x410.exe
-		/mnt/c/WINDOWS/system32/cmd.exe /C x410.exe /wm
-		echo x410 loaded.
+  if test -x $W/AppData/Local/Microsoft/WindowsApps/x410.exe
+    if /mnt/c/WINDOWS/system32/tasklist.exe | grep -iq x410
+      echo x410 already loaded.
+    else
+		  /mnt/c/WINDOWS/system32/cmd.exe /C x410.exe /wm
+		  echo x410 loaded.
+    end
 	else
 		echo x410 not found.
 	end
