814f77013893c814a1be7637d1c1faae51c84636
Duy K. Bui <duy@buifamily.info>
2018-10-24 12:50:11 -0400

    fix(arch): Install sublime from official repo

--- arch-sublime.sh
+++ arch-sublime.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-yay -S --needed --noconfirm sublime-text-3-imfix
+cd (mktemp --directory)    
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg
+sudo pacman-key --add sublimehq-pub.gpg
+sudo pacman-key --lsign-key 8A8F901A
+grep -q download.sublimetext.com /etc/pacman.conf || echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu --needed sublime-text sublime-merge
