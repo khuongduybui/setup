79fed6d980b8f9a25cb7b037e35678488fe95b0f
Duy K. Bui <duybui@amazon.com>
2018-10-24 12:35:46 -0400

    feat(sublime): Also install sublime-merge

--- wlinux-sublime.sh
+++ wlinux-sublime.sh
@@ -3,4 +3,4 @@
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y
-sudo apt install -y sublime-text
+sudo apt install -y sublime-text sublime-merge
