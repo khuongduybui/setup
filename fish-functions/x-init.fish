142b9cdc701c4187b56eccdbdfbceabd32b076c1
Duy K. Bui <duy@buifamily.info>
2018-10-25 03:01:59 +0000

    fix(fish): Temp workaround for cmd /C x410 /wm not returning

--- fish-functions/x-init.fish
+++ fish-functions/x-init.fish
@@ -1,10 +1,11 @@
+# Defined in /tmp/fish.VK565n/x-init.fish @ line 2
 function x-init
 	set_color $fish_color_operator; echo Launching X-Server; set_color normal
   if test -x $W/AppData/Local/Microsoft/WindowsApps/x410.exe
     if /mnt/c/WINDOWS/system32/tasklist.exe | grep -iq x410
       echo x410 already loaded.
     else
-		  /mnt/c/WINDOWS/system32/cmd.exe /C x410.exe /wm
+		  nohup /mnt/c/WINDOWS/system32/cmd.exe /C x410.exe /wm > (mktemp) &
 		  echo x410 loaded.
     end
 	else
