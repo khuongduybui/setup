9b7f82493cf48c692e62cc42a0d150a822e34366
Duy K. Bui <duybui@amazon.com>
2018-11-15 15:28:59 -0500

    fix(font-init): Typos all around

--- fish-functions/font-init.fish
+++ fish-functions/font-init.fish
@@ -1,6 +1,5 @@
-# Defined in /tmp/fish.hPitFj/font-init.fish @ line 2
 function font-init
-    set_color $fish_color_operator; echo Checking Windows fonts into WSL; set_color normal
+	set_color $fish_color_operator; echo Checking Windows fonts into WSL; set_color normal
     sudo mkdir -p /etc/fonts
     if test -f /etc/fonts/local.conf
         echo -n User fonts...
@@ -8,7 +7,7 @@ function font-init
             echo Done
         else
             echo -n Adding...
-            sudo sed -i "s/<fontconfig>/<fontconfig>\n<dir>$W/AppData/Local/Microsoft/Windows/Fonts</dir>" /etc/wsl.conf
+            sudo sed -i "s|<fontconfig>|<fontconfig>\n    <dir>$W/AppData/Local/Microsoft/Windows/Fonts</dir>|" /etc/fonts/local.conf
             echo Done
         end
         echo -n System fonts...
@@ -16,15 +15,15 @@ function font-init
             echo Done
         else
             echo -n Adding...
-            sudo sed -i "s/<fontconfig>/<fontconfig>\n<dir>$WROOT/c/Windows/Fonts</dir>" /etc/wsl.conf
+            sudo sed -i "s|<fontconfig>|<fontconfig>\n    <dir>$WROOT/c/Windows/Fonts</dir>|" /etc/fonts/local.conf
             echo Done
         end
     else
         echo '<?xml version="1.0"?>' | sudo tee /etc/fonts/local.conf
         echo '<!DOCTYPE fontconfig SYSTEM "fonts.dtd">' | sudo tee -a /etc/fonts/local.conf
         echo '<fontconfig>' | sudo tee -a /etc/fonts/local.conf
-        echo "<dir>$WROOT/c/Windows/Fonts</dir>" | sudo tee -a /etc/fonts/local.conf
-        echo "<dir>$W/AppData/Local/Microsoft/Windows/Fonts</dir>" | sudo tee -a /etc/fonts/local.conf
+        echo "    <dir>$WROOT/c/Windows/Fonts</dir>" | sudo tee -a /etc/fonts/local.conf
+        echo "    <dir>$W/AppData/Local/Microsoft/Windows/Fonts</dir>" | sudo tee -a /etc/fonts/local.conf
         echo '</fontconfig>' | sudo tee -a /etc/fonts/local.conf
         echo Done
     end
