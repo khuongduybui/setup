12b5e6d9059422642abdcb9ae89fb70f9946395a
Duy K. Bui <duybui@amazon.com>
2018-11-19 12:21:27 -0500

    feat(fish): rsync-aws also copy to dev desktop as needed

--- fish-functions/rsync-aws.fish
+++ fish-functions/rsync-aws.fish
@@ -1,11 +1,14 @@
 function rsync-aws
-    mkdir -p ~/.aws
+	mkdir -p ~/.aws
     test -d ~/OneDrive; and rsync -r ~/OneDrive/Essentials/dotfile.aws/* ~/.aws
     test -d $WHOME/OneDrive; and rsync -r $WHOME/OneDrive/Essentials/dotfile.aws/* ~/.aws
     test -d $WROOT/d/OneDrive; and rsync -r $WROOT/d/OneDrive/Essentials/dotfile.aws/* ~/.aws
     # test -d $WROOT/w/My\ Documents; and rsync -r $WROOT/w/My\ Documents/Essentials/dotfile.aws/* ~/.aws
 
-    ls -R ~/.local/lib | grep -q BenderLibIsengard; and sed -i 's/;isengard = isengard.plugin/isengard = isengard.plugin/' ~/.aws/config
-    ls -R ~/.local/lib | grep -q GoshawkBotocore; and sed -i 's/;goshawk = goshawk/goshawk = goshawk/' ~/.aws/config
+    if test -f ~/.aws/config
+        ls -R ~/.local/lib | grep -q BenderLibIsengard; and sed -i 's/;isengard = isengard.plugin/isengard = isengard.plugin/' ~/.aws/config
+        ls -R ~/.local/lib | grep -q GoshawkBotocore; and sed -i 's/;goshawk = goshawk/goshawk = goshawk/' ~/.aws/config
+        host duybui.aka.amazon.com >/dev/null; and scp ~/.aws/* duybui.aka.amazon.com:~/.aws/
+    end
     echo Done
 end
