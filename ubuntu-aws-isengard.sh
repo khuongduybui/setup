b3efaa47971ec25d4993cb2a38b4fab1eb3ee31a
Duy K. Bui <duybui@amazon.com>
2018-11-19 13:23:05 -0500

    feat(linux): AWS CLI installation script can be universal now

--- ubuntu-aws-isengard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pip install --user awscli
-pip install --user git+ssh://git.amazon.com/pkg/BenderLibIsengard
-pip install --user git+ssh://git.amazon.com/pkg/GoshawkBotocore@mainline-1.1
