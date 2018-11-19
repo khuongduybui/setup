12c32064613a11ac8ccdc5a00c66cc27cb941fd7
Duy K. Bui <duybui@amazon.com>
2018-11-19 11:31:56 -0500

    fix(fish): ips now works on Ubuntu / WLinux

--- fish-functions/ips.fish
+++ fish-functions/ips.fish
@@ -1,6 +1,7 @@
-# Defined in /tmp/fish.yralYS/ips.fish @ line 2
 function ips
 	set -lx AWS_PROFILE $argv[1]
+  test -e /etc/ssl/certs/ca-certificates.crt; and set -lx REQUESTS_CA_BUNDLE /etc/ssl/certs/ca-certificates.crt
+  test -e /etc/ssl/certs/ca-bundle.crt; and set -lx REQUESTS_CA_BUNDLE /etc/ssl/certs/ca-bundle.crt
 	if __is_mac
 		/usr/local/bin/fish
 	else
