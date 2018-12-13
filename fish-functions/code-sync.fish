11b70538b1348d639c4faac480acd40d914d6a3c
Duy K. Bui <duy@buifamily.info>
2018-12-12 19:57:19 -0500

    fix(fish): slightly better code-sync

--- fish-functions/code-sync.fish
+++ fish-functions/code-sync.fish
@@ -1,13 +1,14 @@
+# Defined in /tmp/fish.6YRvIr/code-sync.fish @ line 2
 function code-sync
-    cd ~/code
+	cd ~/code
     for workspace in (ls | grep -v .code-workspace)
-        echo -n Syncing $workspace...
+        echo -n "Syncing $workspace... "
         echo '{"folders":[' > $workspace.code-workspace
         for package in (ls $workspace/src)
             echo '{"path": "'$workspace'\\\\src\\\\'$package'"},' >> $workspace.code-workspace
         end
         echo ']}' >> $workspace.code-workspace
-        echo done
+        echo 'done'
     end
     cd -
 end
