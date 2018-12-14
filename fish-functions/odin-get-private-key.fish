4df580feca16cc1b38034f6bf32b23ca49794326
Duy K. Bui <duybui@amazon.com>
2018-12-14 13:18:25 -0500

    fix(fish): more reliable odin-get-private-key

--- fish-functions/odin-get-private-key.fish
+++ fish-functions/odin-get-private-key.fish
@@ -1,13 +1,11 @@
-# Defined in /tmp/fish.LkHiTj/odin-get-private-key.fish @ line 2
+# Defined in /tmp/fish.NesbaD/odin-get-private-key.fish @ line 2
 function odin-get-private-key
 	if test (which odin-get) >/dev/null
 		echo "Backing up existing file if any"
 		test -f ~/.ssh/$argv[1].pem.bak; and rm ~/.ssh/$argv[1].pem.bak
 		test -f ~/.ssh/$argv[1].pem; and mv ~/.ssh/$argv[1].pem ~/.ssh/$argv[1].pem.bak
 		echo "Fetching $argv[1] as ~/.ssh/$argv[1].pem"
-		echo "-----BEGIN RSA PRIVATE KEY-----" > ~/.ssh/$argv[1].pem
-		odin-get -t PrivateKey $argv[1] | base64 >> ~/.ssh/$argv[1].pem
-		echo "-----END RSA PRIVATE KEY-----" >> ~/.ssh/$argv[1].pem
+		odin-get -t PrivateKey $argv[1] | openssl pkcs8 -nocrypt -inform DER -outform PEM > ~/.ssh/$argv[1].pem
 		chmod 600 ~/.ssh/$argv[1].pem
 	else
 		echo "odin-get not found"
