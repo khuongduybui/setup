c132130c9c14a5160638127237756d0293723146
Duy K. Bui <duy@buifamily.info>
2019-01-03 12:20:14 -0500

    fix(fish): Allow bypassing dbus-dependent editors

--- config.fish
+++ config.fish
@@ -30,12 +30,12 @@ if test -z $INIT
 
     ### Editors
     set_color $fish_color_operator; echo Searching for Editors; set_color normal
-    if not __is_dev_desktop
+    if not __is_dev_desktop; and not test -e ~/.disable-dbus
         which dbus-launch >/dev/null 2>&1; and test -f ~/.config/fish/functions/bass.fish; and bass (dbus-launch --auto-syntax)
     end
-    if which code >/dev/null 2>&1; and not test -e ~/.disable-vscode
+    if which code >/dev/null 2>&1; and not test -e ~/.disable-vscode; and not test -e ~/.disable-dbus
         set -x EDITOR (which code)' -nw'
-    else if which subl >/dev/null 2>&1; and not test -e ~/.disable-sublime
+    else if which subl >/dev/null 2>&1; and not test -e ~/.disable-sublime; and not test -e ~/.disable-dbus
         set -x EDITOR (which subl)' -nw'
     else
         set -x EDITOR (which io.elementary.code 2>/dev/null; or which micro 2>/dev/null; or which nvim 2>/dev/null; or which vim 2>/dev/null; or which vi 2>/dev/null; or which nano 2>/dev/null)
