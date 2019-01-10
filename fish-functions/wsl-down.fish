b3ae816b8981a3dd707aaf5a69c23cfd78bbfc21
Duy K. Bui <duy@buifamily.info>
2019-01-09 23:08:42 -0500

    feat(fish): use wsl.exe for wsl-down

--- fish-functions/wsl-down.fish
+++ fish-functions/wsl-down.fish
@@ -1,9 +1,9 @@
 function wsl-down
 	if __read_confirm --title 'Shut down'
         if test -z $argv[1]
-            wslconfig.exe /list
+            wsl.exe --list
         else
-            wslconfig.exe /terminate $argv[1]
+            wsl.exe --terminate $argv[1]
         end
     end
 end
