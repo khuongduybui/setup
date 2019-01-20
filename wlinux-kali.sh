715e02a5393bee09e7951f02ac5d8bafe024aeba
Duy K. Bui <duybui@amazon.com>
2019-01-19 21:06:02 -0500

    fix(wlinux): import Kali key from plain-text http Ubuntu keyserver

--- wlinux-kali.sh
+++ wlinux-kali.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-gpg --keyserver pgpkeys.mit.edu --recv-key  ED444FF07D8D0BF6
-gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv ED444FF07D8D0BF6
 echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' | sudo tee /etc/apt/sources.list.d/kali.list
 echo '#deb-src http://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee -a /etc/apt/sources.list.d/kali.list
 echo 'Package: *' | sudo tee /etc/apt/preferences.d/kali.pref
