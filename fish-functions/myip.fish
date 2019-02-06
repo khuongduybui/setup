3cde81edc9a94c719cae07d158c99b64c8ccd0b1
Duy K. Bui <duy@buifamily.info>
2019-02-06 09:28:50 -0500

    fix: Use ip instead of ifconfig where present

--- fish-functions/myip.fish
+++ fish-functions/myip.fish
@@ -1,10 +1,14 @@
-# Defined in /tmp/fish.nB8kGV/myip.fish @ line 2
+# Defined in /tmp/fish.uTlQbQ/myip.fish @ line 2
 function myip
 	which curl >/dev/null 2>&1; and segment blue white " External IP "; and segment_close; and curl -s https://www.myexternalip.com/raw | grep .
 	segment blue white " Internal IP "; segment_close; echo;
-	if __is_mac
-		ifconfig | fgrep inet | grep --color -E " ([0-9a-f]*[\.:]?)+[%a-z0-9]* "
+	if which ip >/dev/null 2>&1
+	  ip address | grep inet
 	else
-		ifconfig | fgrep inet | grep --color -E "[ :]([0-9a-f]*[.:])+[%a-z0-9/]*[ \$]"
-	end
+    if __is_mac
+		  ifconfig | fgrep inet | grep --color -E " ([0-9a-f]*[\.:]?)+[%a-z0-9]* "
+  	else
+  		ifconfig | fgrep inet | grep --color -E "[ :]([0-9a-f]*[.:])+[%a-z0-9/]*[ \$]"
+	  end
+  end
 end
