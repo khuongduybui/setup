81a71ab63eb7c5a940a6e2eb83602cf44bde42b3
Duy K. Bui <duy@buifamily.info>
2019-02-27 21:50:21 -0500

    feat: Add linux-vscode-liveshare.sh

---
+++ linux-vscode-liveshare.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# From https://docs.microsoft.com/en-us/visualstudio/liveshare/reference/linux#install-linux-prerequisites
+wget -O /tmp/vsls-reqs https://aka.ms/vsls-linux-prereq-script
+bash /tmp/vsls-reqs
