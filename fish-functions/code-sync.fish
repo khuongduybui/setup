4b7384c785e482a8a4765815e54cb9162b84b7b8
Duy K. Bui <duy@buifamily.info>
2019-03-04 23:02:54 -0500

    fix: No longer using src/ in code-sync for non-Amazon projects

--- fish-functions/code-sync.fish
+++ fish-functions/code-sync.fish
@@ -1,14 +1,14 @@
-# Defined in /tmp/fish.mp9lL0/code-sync.fish @ line 2
+# Defined in /tmp/fish.E2IYb6/code-sync.fish @ line 2
 function code-sync
-    cd ~/code
+	cd ~/code
     for workspace in (ls | grep -v .code-workspace)
         echo -n "Syncing $workspace... "
         echo '{"folders":[' >"$workspace.code-workspace"
         if test -e "$workspace/packageInfo"
             echo '{"path": "'$workspace'"},' >>"$workspace.code-workspace"
         else
-            for package in (ls "$workspace/src")
-                echo '{"path": "'$workspace"\\\\src\\\\"$package'"},' >>"$workspace.code-workspace"
+            for package in (ls "$workspace/")
+                echo '{"path": "'$workspace"\\\\"$package'"},' >>"$workspace.code-workspace"
             end
         end
         echo ']}' >>"$workspace.code-workspace"
