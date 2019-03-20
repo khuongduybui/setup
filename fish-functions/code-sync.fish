32ba8083a76ffac9574979d312fc628f2609489a
Duy K. Bui <duy@buifamily.info>
2019-03-19 23:21:16 -0400

    fix(fish): Use forward slash in code-sync

--- fish-functions/code-sync.fish
+++ fish-functions/code-sync.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.E2IYb6/code-sync.fish @ line 2
+# Defined in /tmp/fish.UhGFP6/code-sync.fish @ line 2
 function code-sync
 	cd ~/code
     for workspace in (ls | grep -v .code-workspace)
@@ -8,7 +8,7 @@ function code-sync
             echo '{"path": "'$workspace'"},' >>"$workspace.code-workspace"
         else
             for package in (ls "$workspace/")
-                echo '{"path": "'$workspace"\\\\"$package'"},' >>"$workspace.code-workspace"
+                echo '{"path": "'$workspace/$package'"},' >>"$workspace.code-workspace"
             end
         end
         echo ']}' >>"$workspace.code-workspace"
