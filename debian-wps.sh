6e9c7da15769d09b81c5fb715fec32b7a29a1586
Duy K. Bui <duy@buifamily.info>
2019-03-24 00:55:24 -0400

    fix(linux): Update WPS to latest version

--- debian-wps.sh
+++ debian-wps.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # From http://wps-community.org/downloads
-wget http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_10.1.0.6757_amd64.deb -O /tmp/wps.deb
+wget http://kdl.cc.ksosoft.com/wps-community/download/6758/wps-office_10.1.0.6758_amd64.deb -O /tmp/wps.deb
 sudo apt install -y xdg-utils
 sudo apt install -y /tmp/wps.deb
