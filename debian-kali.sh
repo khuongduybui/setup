1d08ea3033ff34a97940fcb70fd14e5afe04903a
Duy K. Bui <duybui@amazon.com>
2019-04-18 14:30:08 -0400

    feat: Update repo keys in pengwin.sh

--- debian-kali.sh
+++ debian-kali.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv ED444FF07D8D0BF6
-echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' | sudo tee /etc/apt/sources.list.d/kali.list
-echo '#deb-src http://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee -a /etc/apt/sources.list.d/kali.list
-echo 'Package: *' | sudo tee /etc/apt/preferences.d/kali.pref
-echo 'Pin: release a=kali-rolling' | sudo tee -a /etc/apt/preferences.d/kali.pref
-echo 'Pin-Priority: 1' | sudo tee -a /etc/apt/preferences.d/kali.pref
-sudo apt update -y
+if ! [ -e /etc/apt/sources.list.d/kali.list ]; then
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv ED444FF07D8D0BF6
+    echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' | sudo tee /etc/apt/sources.list.d/kali.list
+    echo '#deb-src http://http.kali.org/kali kali-rolling main non-free contrib' | sudo tee -a /etc/apt/sources.list.d/kali.list
+    echo 'Package: *' | sudo tee /etc/apt/preferences.d/kali.pref
+    echo 'Pin: release a=kali-rolling' | sudo tee -a /etc/apt/preferences.d/kali.pref
+    echo 'Pin-Priority: 1' | sudo tee -a /etc/apt/preferences.d/kali.pref
+    sudo apt update -y
+fi
--- pengwin.sh
+++ pengwin.sh
@@ -22,6 +22,11 @@ test -x /usr/bin/pip && sudo /usr/bin/pip install --upgrade pip
 test -x /usr/bin/pip2 && sudo /usr/bin/pip2 install --upgrade pip
 
 echo "--- Update system ---"
+test -e /etc/apt/sources.list.d/yarn.list && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+test -e /etc/apt/sources.list.d/openjdk.list && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 86F44E2A
+test -e /etc/apt/sources.list.d/kali.list && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv ED444FF07D8D0BF6
+test -e /etc/apt/sources.list.d/amazon.list && wget --no-check-certificate -qO - https://cascadia.corp.amazon.com/amazon/clienteng.gpg | sudo apt-key add -
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
