1470852823ff6f7efd6e329bb56e3c71592e9a5f
Duy K. Bui <duybui@amazon.com>
2019-05-18 12:02:29 -0400

    fix: change typo in fish init script

--- config.fish
+++ config.fish
@@ -52,8 +52,8 @@ if test -z $INIT
     end
 
     ### Languages
-    grep -q -e "^LC_ALL=en_US.UTF-8\$" /etc/environment || echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
-    grep -q -e "^en_US.UTF-8 UTF-8\$" /etc/locale.gen || echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+    grep -q -e "^LC_ALL=en_US.UTF-8\$" /etc/environment; or echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/environment
+    grep -q -e "^en_US.UTF-8 UTF-8\$" /etc/locale.gen; or echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
     echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
     sudo locale-gen en_US.UTF-8
 
