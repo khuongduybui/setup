79f89a5d10de764361701c3bb2d4727d010c61ed
Duy K. Bui <duybui@amazon.com>
2019-05-18 19:05:26 -0400

    feat: Add apll and c2l (cd to logs)

---
+++ fish-functions/c2l.fish
@@ -0,0 +1,56 @@
+# Defined in /tmp/fish.Tumsnw/c2l.fish @ line 2
+function c2l
+	if not test -d ~/code
+		set_color $fish_color_error
+		echo -n "ERROR: "
+		set_color normal
+		set_color $fish_color_param
+		echo -n "code/ "
+		set_color $fish_color_error
+		echo -n "not found under "
+		set_color normal
+		set_color $fish_color_param
+		echo "~/"
+		return
+	end
+	pushd .
+	cd ~/code
+	if not test (count $argv) = 0
+		if not test -d ./$argv[1]
+			set_color $fish_color_error
+			echo -n "ERROR: "
+			set_color normal
+			set_color $fish_color_param
+			echo -n "$argv[1]/ "
+			set_color $fish_color_error
+			echo -n "not found under "
+			set_color normal
+			set_color $fish_color_param
+			echo "~/code/"
+			set_color normal
+		else
+			cd $argv[1]
+			set -xg DEBUG "*$argv[1]*:*"
+			if test -d ./logs
+				cd logs
+				set -gx BRAZIL_PACKAGES_DIR (pwd)
+				if test -d ./BitScripts
+					bass source ./BitScripts/envsetup.sh
+				end
+			end
+			if test (count $argv) -gt 1
+				cd ./(command ls | command grep --color=none -i $argv[2..-1])
+			else if test (count (command ls)) = 1
+				cd (command ls)
+			end
+		end
+	end
+	if test -d ./.git
+		git status
+	else
+		l
+	end
+	if test -f ./env/bin/activate.fish
+		. ./env/bin/activate.fish
+	end
+end
