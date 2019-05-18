20059ddc837005575cccb734ec7bea950e392ec0
Duy K. Bui <duybui@amazon.com>
2019-05-18 12:15:35 -0400

    fix: wsl-down now only lists running distros

--- fish-functions/wsl-down.fish
+++ fish-functions/wsl-down.fish
@@ -1,7 +1,8 @@
+# Defined in /tmp/fish.5aSU11/wsl-down.fish @ line 2
 function wsl-down
 	if __read_confirm --title 'Shut down'
         if test -z $argv[1]
-            wsl.exe --list
+            wsl.exe --list --running
         else
             wsl.exe --terminate $argv[1]
         end
