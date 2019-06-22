75a3b78e2c5cea871f81bccca008661b5d3bb0fb
Duy K. Bui <duybui@amazon.com>
2019-06-22 15:25:14 -0400

    fix: consolidate al2/rhel ruby

--- al2-ruby.sh
+++ al2-ruby.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 if ! [ -e ~/.rbenv/bin/rbenv ]; then
+    sudo yum install -y openssl-devel readline-devel zlib-devel
     curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
     ~/.rbenv/bin/rbenv install 2.5.3
     ~/.rbenv/bin/rbenv global 2.5.3
--- rhel-ruby.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo yum install -y openssl-devel readline-devel zlib-devel
-bash ~/setup/al2-ruby.sh
