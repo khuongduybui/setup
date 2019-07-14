6ae599e72cd6b91ca6408e56cf1ef06cb8a90f63
Duy K. Bui <duy@buifamily.info>
2019-07-14 14:45:41 -0400

    fix: better default gitconfig

--- debian-win.sh
+++ debian-win.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 if ! [ -e /etc/apt/sources.list.d/whitewaterfoundry_wslu.list ]; then
-    curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/wslu/script.deb.sh | sudo bash
-    sudo apt install -y wslu
+    if ! [ -e /etc/apt/sources.list.d/whitewaterfoundry.list ]; then
+        curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/wslu/script.deb.sh | sudo bash
+        sudo apt install -y wslu
+    fi
 fi
--- fish-functions/rsync-aws.fish
+++ fish-functions/rsync-aws.fish
@@ -1,7 +1,17 @@
+# Defined in /tmp/fish.oik5gT/rsync-aws.fish @ line 2
 function rsync-aws
 	mkdir -p ~/.aws
-    test -d ~/OneDrive; and rsync -r ~/OneDrive/Essentials/dotfile.aws/* ~/.aws
-    test -d $WHOME/OneDrive; and rsync -r $WHOME/OneDrive/Essentials/dotfile.aws/* ~/.aws
-    test -d (wslpath 'D:/OneDrive'); and rsync -r (wslpath 'D:/OneDrive/Essentials/dotfile.aws')/* ~/.aws
+
+    test -d ~/OneDrive
+    and rsync -r ~/OneDrive/Essentials/dotfile.aws/* ~/.aws
+
+    test -d $WHOME/OneDrive
+    and rsync -r $WHOME/OneDrive/Essentials/dotfile.aws/* ~/.aws
+
+    test -d (wslpath 'D:/OneDrive')
+    and rsync -r (wslpath 'D:/OneDrive/Essentials/dotfile.aws')/* ~/.aws
+
+    chmod 644 ~/.aws/*
+
     echo Done
 end
