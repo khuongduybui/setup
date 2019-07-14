cecc041f65b64a4726c79f100c4fba65e3d1c81c
Duy K. Bui <duy@buifamily.info>
2019-07-14 15:48:07 -0400

    fix: also chmod known_hosts and authorized_keys

--- fish-functions/rsync-ssh.fish
+++ fish-functions/rsync-ssh.fish
@@ -1,11 +1,21 @@
+# Defined in /tmp/fish.QqRcnu/rsync-ssh.fish @ line 2
 function rsync-ssh
-    mkdir -p ~/.ssh
-    test -d ~/OneDrive; and rsync -r ~/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
-    test -d $WHOME/OneDrive; and rsync -r $WHOME/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
-    test -d (wslpath 'D:/OneDrive'); and rsync -r (wslpath 'D:/OneDrive/Essentials/dotfile.ssh')/* ~/.ssh
+	mkdir -p ~/.ssh
+
+    test -d ~/OneDrive
+    and rsync -r ~/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
+
+    test -d $WHOME/OneDrive
+    and rsync -r $WHOME/OneDrive/Essentials/dotfile.ssh/* ~/.ssh
+
+    test -d (wslpath 'D:/OneDrive')
+    and rsync -r (wslpath 'D:/OneDrive/Essentials/dotfile.ssh')/* ~/.ssh
+
     chmod 600 ~/.ssh/*.pem
     chmod 600 ~/.ssh/config
     chmod 644 ~/.ssh/*.pub
-    chmod 600 ~/.ssh/*.pem-cert.pub
+    chmod 644 ~/.ssh/known_hosts
+    chmod 644 ~/.ssh/authorized_keys
+
     echo Done
 end
