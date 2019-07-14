0815b85c9227e4bcb1b63214e542cbd44758eb44
Duy K. Bui <duy@buifamily.info>
2019-07-14 14:49:00 -0400

    fix: shutdown all linux with wsl-down by default

--- fish-functions/wsl-down.fish
+++ fish-functions/wsl-down.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.5aSU11/wsl-down.fish @ line 2
+# Defined in /tmp/fish.zJ1uQW/wsl-down.fish @ line 2
 function wsl-down
 	if __read_confirm --title 'Shut down'
         if test -z $argv[1]
-            wsl.exe --list --running
+            wsl.exe --shutdown
         else
             wsl.exe --terminate $argv[1]
         end
