59f7380fd54c7e839ff9464f636cdf3b10574a4d
Duy K. Bui <duy@buifamily.info>
2019-07-16 16:38:31 -0400

    fix: more reliable debian-mongodb on WSL

--- debian-mongodb.sh
+++ debian-mongodb.sh
@@ -4,7 +4,15 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu $(lsb_release -cs | sed 's/juno/bionic/' | sed 's/buster/bionic/')/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt update -y
-sudo apt install -y mongodb-org
 
-grep -q -i "Microsoft" /proc/version && sudo cp ~/setup/mongodb.init.d /etc/init.d/mongodb                  # systemd not supported in WSL
-grep -q -i "Microsoft" /proc/version && sudo cp ~/setup/mongodb.service /lib/systemd/system/mongodb.service # systemd not supported in WSL
+if grep -q -i "Microsoft" /proc/version; then
+  # systemd not supported in WSL
+  sudo apt install -y mongodb
+  sudo apt remove -y mongodb
+  sudo apt autoremove -y
+# sudo cp ~/setup/mongodb.init.d /etc/init.d/mongodb
+# sudo cp ~/setup/mongodb.service /lib/systemd/system/mongodb.service
+fi
+
+sudo apt install -y mongodb-org
+sudo service mongodb start
