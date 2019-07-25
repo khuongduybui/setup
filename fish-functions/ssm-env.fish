32d662a42e501cb5090440e92ab8cffb0b0d9d15
Duy K. Bui <duy@buifamily.info>
2019-07-25 11:58:15 -0400

    feat: Use menu in ssm-env

--- fish-functions/ssm-env.fish
+++ fish-functions/ssm-env.fish
@@ -1,4 +1,22 @@
-# Defined in /tmp/fish.h3rf9C/ssm-env.fish @ line 2
+# Defined in /tmp/fish.5vzJ5j/ssm-env.fish @ line 2
 function ssm-env
-    aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances | map(select(.AutoScalingGroupName == "'$argv[1]'")) | .[].InstanceId'
+	set -l asg
+    if test (count $argv) = 1
+        set -x asg $argv[1]
+    else
+        set -l asgs (aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances[].AutoScalingGroupName')
+        set -l menu_cursor_glyph \uf7c2
+        menu $asgs
+        set -x asg $asgs[$menu_selected_index]
+    end
+    echo 'ASG:      '\uf7c2 $asg
+
+    set -l instanceId
+    set -l instances (aws autoscaling describe-auto-scaling-instances | jq -r '.AutoScalingInstances | map(select(.AutoScalingGroupName == "'$asg'")) | .[].InstanceId')
+    set -l menu_cursor_glyph \uf878
+    menu $instances
+    set -l instanceId $instances[$menu_selected_index]
+    echo 'Instance: '\uf878 $instanceId
+
+    aws ssm start-session --target $instanceId
 end
