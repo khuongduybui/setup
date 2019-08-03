1b2f4dfe93237a1d930e2c212228f28d597ecd7e
Duy K. Bui <duy@buifamily.info>
2019-08-03 17:42:04 -0400

    fix: linux-inotify should re-apply settings on-demand

--- linux-inotify.sh
+++ linux-inotify.sh
@@ -4,5 +4,5 @@ which apt >/dev/null 2>&1 && sudo apt install -y inotify-tools
 which yum >/dev/null 2>&1 && sudo yum install -y inotify-tools
 if ! grep -q /etc/sysctl.conf -e "fs.inotify.max_user_watches"; then
     echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf
-    sudo sysctl -p
 fi
+sudo sysctl -p
