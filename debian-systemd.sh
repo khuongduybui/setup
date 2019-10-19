3184ae068b434844a3b695d4e74f65d677ca122c
Duy K. Bui <duy@buifamily.info>
2019-10-19 19:00:34 -0400

    feat: Add dotnet-core repo when install systemd genie

--- debian-systemd.sh
+++ debian-systemd.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-curl -s https://packagecloud.io/install/repositories/arkane-systems/wsl-translinux/script.deb.sh | sudo bash
+rm -f /tmp/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
+
+curl -s https://packagecloud.io/install/repositories/arkane-systems/wsl-translinux/script.deb.sh > /tmp/genie.sh
+os=ubuntu dist=bionic sudo -E bash /tmp/genie.sh
+
 sudo apt update -y
 sudo apt install -y systemd-genie
