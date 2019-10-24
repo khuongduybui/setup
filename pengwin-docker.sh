56182c37db9fd88f5d3a83b6cbec15c29204175e
Duy K. Bui <duy@buifamily.info>
2019-10-23 21:35:08 -0400

    feat: docker without sudo

--- pengwin-docker.sh
+++ pengwin-docker.sh
@@ -6,3 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt install docker-ce
+
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
