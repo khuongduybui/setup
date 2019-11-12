9b039fab97bd442c0af96568a6d56f60ee440d2c
Duy K. Bui <duy@buifamily.info>
2019-11-12 10:05:07 -0500

    feat: Use symlink in place of rsync for dotfile.aws

--- fish-functions/rsync-aws.fish
+++ fish-functions/rsync-aws.fish
@@ -1,15 +1,13 @@
-# Defined in /tmp/fish.oik5gT/rsync-aws.fish @ line 2
+# Defined in /tmp/fish.X0qdGd/rsync-aws.fish @ line 2
 function rsync-aws
-    mkdir -p ~/.aws
-
-    test -d ~/OneDrive
-    and rsync -r ~/OneDrive/Essentials/dotfile.aws/* ~/.aws
+	test -d ~/OneDrive
+    and bash ~/setup/backup-and-link.sh ~/OneDrive/Essentials/dotfile.aws ~/.aws
 
     test -d $WHOME/OneDrive
-    and rsync -r $WHOME/OneDrive/Essentials/dotfile.aws/* ~/.aws
+    and bash ~/setup/backup-and-link.sh $WHOME/OneDrive/Essentials/dotfile.aws ~/.aws
 
     test -d (wslpath 'D:/OneDrive')
-    and rsync -r (wslpath 'D:/OneDrive/Essentials/dotfile.aws')/* ~/.aws
+    and bash ~/setup/backup-and-link.sh (wslpath 'D:/OneDrive/Essentials/dotfile.aws') ~/.aws
 
     chmod 644 ~/.aws/*
 
