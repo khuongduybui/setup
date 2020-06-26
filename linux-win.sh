5d513452d1ac8b27ce58ca0c7d6da92c711a6cd3
Duy K. Bui <duy@buifamily.info>
2020-06-26 00:28:19 -0400

    feat: suppress wsl.conf

--- linux-win.sh
+++ linux-win.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-bash ~/setup/backup-and-copy.sh ~/setup/wsl.conf /etc/
+# bash ~/setup/backup-and-copy.sh ~/setup/wsl.conf /etc/
 
 which update-alternatives && sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser $(which explorer.exe) 1
 which update-alternatives && sudo update-alternatives --install /usr/bin/www-browser www-browser $(which explorer.exe) 1
