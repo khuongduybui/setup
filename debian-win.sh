deacc0e49236876d44a08226643597b3c7cac9d2
Duy K. Bui <duy@buifamily.info>
2020-09-08 10:46:22 -0400

    feat: No longer need to install wslu

--- debian-win.sh
+++ debian-win.sh
@@ -1,8 +1,1 @@
 #! /bin/bash
-
-if ! [ -e /etc/apt/sources.list.d/whitewaterfoundry_wslu.list ]; then
-    if ! [ -e /etc/apt/sources.list.d/whitewaterfoundry.list ]; then
-        curl -s https://packagecloud.io/install/repositories/whitewaterfoundry/wslu/script.deb.sh | sudo bash
-        sudo apt install -y wslu
-    fi
-fi
