094cd3729a81a40e45a7c04a41af23c333be4848
Duy K. Bui <duy@buifamily.info>
2020-09-30 12:23:43 -0400

    feat: Support aarch64 in linux-aws-cli-v2

--- linux-aws-cli-v2.sh
+++ linux-aws-cli-v2.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # From https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+ARCH=`uname -m`
+curl "https://awscli.amazonaws.com/awscli-exe-linux-$ARCH.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
