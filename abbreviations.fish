e42fc37dc842eb13dced397aed9f325b54fa4333
Duy K. Bui <duy@buifamily.info>
2020-10-02 14:58:52 -0400

    feat: Add a few abbr for ssh

--- abbreviations.fish
+++ abbreviations.fish
@@ -2,8 +2,10 @@
 
 abbr my-setup 'curl -L https://gist.github.com/khuongduybui/a6d9d7867a89f304b5b774af9e6e6257/raw/setup.sh | bash -'
 abbr fisher-update 'fisher self-update; fisher'
-abbr update-git 'git remote set-url origin git@github.com:khuongduybui/setup.git'
 abbr ssh-ec2 'ssh -i ~/.ssh/ec2.pem -l ec2-user'
+abbr ssh-raw 'ssh -F /dev/null'
+abbr sshp 'ssh -o PreferredAuthentications=password'
+abbr update-git 'git remote set-url origin git@github.com:khuongduybui/setup.git'
 abbr update-git-ll 'git config user.email "duy.bui@lunarline.com"; git config user.signingkey 083EF93045D6B22C'
 abbr update-git-ct 'git config user.email "duy.bui@clevertech.biz"; git config user.signingkey 603790593AB493EA'
 abbr update-git-msi 'git config user.email "duy.bui@motorolasolutions.com"; git config user.signingkey F9F596B7934A7B17'
@@ -19,7 +21,7 @@ abbr v 'view'
 abbr e 'edit'
 abbr s 'settings'
 abbr cmd command
-abbr cdw 'cd $WHOME'
+abbr cdw 'cd ~/winhome'
 
 abbr bdf 'bit diff --ignore-space-change'
 abbr gdf 'git diff --ignore-space-change'
@@ -41,6 +43,7 @@ abbr y 'yarn'
 abbr yx 'yarn exec'
 abbr ya 'yarn add'
 abbr yad 'yarn add -D'
+abbr ycu yarn upgrade-interactive --latest
 abbr p 'pnpm'
 abbr px 'pnpx'
 abbr pi 'pnpm install'
@@ -48,7 +51,6 @@ abbr pid 'pnpm install --save-dev'
 
 abbr azrcc 'git clone git@ssh.dev.azure.com:v3/LunarlineProducts/'
 
-abbr aps 'aws-shell'
 abbr awscc 'git clone codecommit://'
 abbr ssm 'aws ssm start-session --document-name AWS-StartInteractiveCommand --parameters command="bash -l" --target'
 abbr bsm 'aws ssm start-session --document-name SSM-Bitnami --target'
@@ -65,4 +67,3 @@ abbr fp 'feathers-plus'
 abbr fpg 'feathers-plus generate'
 abbr fpgs 'feathers-plus generate service'
 abbr fpgh 'feathers-plus generate hook'
-abbr ycu yarn upgrade-interactive --latest
