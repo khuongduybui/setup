08a6fc803011356672a14f982217a1bbf191b78a
Duy K. Bui <duy@buifamily.info>
2020-10-17 23:11:07 -0400

    fix: docker-credential-pass latest release

--- linux-docker-pass.sh
+++ linux-docker-pass.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-# curl -s https://api.github.com/repos/adam7/delugia-code/releases/latest | jq -r ".assets[] | select(.name | test(\"omplete.ttf\")) | .browser_download_url" | xargs wget -q -O ~/setup/Delugia.Nerd.Font.Complete.ttf
+# From https://github.com/docker/docker-credential-helpers/releases
 cd /tmp
-wget -q -O ./docker-credential-pass.tar.gz https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz
+rm -rf docker-credential-pass*
+curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest | jq -r ".assets[] | select(.name | test(\"docker-credential-pass\")) | .browser_download_url" | xargs -q -O ./docker-credential-pass.tar.gz
 tar -xzf docker-credential-pass.tar.gz
 mv docker-credential-pass ~/bin
-chmod +x ~/bin/*
+chmod +x ~/bin/docker-credential-pass
