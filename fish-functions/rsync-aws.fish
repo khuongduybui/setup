5de50aea62b3d254e802b63751fc24019c37d2bb
Duy K. Bui <duy@buifamily.info>
2020-10-17 20:44:22 -0400

    fix: better rsync-aws and linux-aws-cli-v2

--- fish-functions/rsync-aws.fish
+++ fish-functions/rsync-aws.fish
@@ -1,6 +1,6 @@
 # Defined in /tmp/fish.0TsA3z/rsync-aws.fish @ line 2
 function rsync-aws
-	test -d ~/OneDrive
+    test -d ~/OneDrive
     and bash ~/setup/backup-and-link.sh ~/OneDrive/Essentials/dotfile.aws ~/.aws
 
     test -d $WHOME/OneDrive
@@ -9,7 +9,8 @@ function rsync-aws
     test -d (wslpath 'D:/OneDrive')
     and bash ~/setup/backup-and-link.sh (wslpath 'D:/OneDrive/Essentials/dotfile.aws') ~/.aws
 
-    chmod 644 ~/.aws/*
+    test -f ~/.aws/config && chmod 644 ~/.aws/config
+    test -f ~/.aws/credentials && chmod 644 ~/.aws/credentials
 
     echo Done
 end
--- linux-aws-cli-v2.sh
+++ linux-aws-cli-v2.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+cd /tmp
+rm -rf ./aws
 # From https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
 ARCH=`uname -m`
 curl "https://awscli.amazonaws.com/awscli-exe-linux-$ARCH.zip" -o "awscliv2.zip"
