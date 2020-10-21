fa95d202575cbc07979f206b4b52a1754b29aa46
Duy K. Bui <duy@buifamily.info>
2020-10-20 20:01:41 -0400

    feat: Add debian-dive.sh

---
+++ debian-dive.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# From https://github.com/wagoodman/dive/releases
+cd /tmp
+rm -rf dive.deb
+curl -s https://api.github.com/repos/wagoodman/dive/releases/latest | jq -r ".assets[] | select(.name | test(\"linux_amd64.deb\")) | .browser_download_url" | xargs -q -O ./dive.deb
+sudo apt install dive.deb
