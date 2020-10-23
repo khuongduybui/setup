ebfe4c5aa072b4c70b793ee577b458d2508c1c89
Duy K. Bui <duy@buifamily.info>
2020-10-23 10:57:39 -0400

    feat: Modernize AL2

--- al2.sh
+++ al2.sh
@@ -3,12 +3,12 @@
 echo "--- Install package manager ---"
 # sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y deltarpm
-sudo amazon-linux-extras install -y epel mate-desktop1.x
+sudo amazon-linux-extras install -y epel
 curl -L https://download.opensuse.org/repositories/utilities/RHEL_7/utilities.repo | sudo tee /etc/yum.repos.d/opensuse-utilities.repo
 
 echo "--- Install system utils ---"
 if [ ! -e /etc/yum.repos.d/fish.repo ]; then
-    curl -L https://download.opensuse.org/repositories/shells:fish:release:2/RHEL_7/shells:fish:release:2.repo | sudo tee /etc/yum.repos.d/fish.repo
+    curl -L https://download.opensuse.org/repositories/shells:fish:release:3/RHEL_7/shells:fish:release:3.repo | sudo tee /etc/yum.repos.d/fish.repo
     sudo yum install -y fish
 fi
 
@@ -20,14 +20,14 @@ sudo yum install -y mc # @TODO: screenfetch
 
 echo "---Install dev tools---"
 sudo yum groupinstall -y "Development Tools"
-sudo yum install -y python2 python2-pip
-bash ~/setup/al2-nodejs.sh
-bash ~/setup/al2-openjdk.sh
-bash ~/setup/al2-python.sh
-bash ~/setup/al2-ruby.sh
-~/.pyenv/shims/pip install --user --no-warn-script-location --upgrade awscli
-test -x /usr/bin/pip && sudo /usr/bin/pip install --upgrade pip
-test -x /usr/bin/pip2 && sudo /usr/bin/pip2 install --upgrade pip
+# sudo yum install -y python2 python2-pip
+# bash ~/setup/al2-nodejs.sh
+# bash ~/setup/al2-openjdk.sh
+# bash ~/setup/al2-python.sh
+# bash ~/setup/al2-ruby.sh
+# ~/.pyenv/shims/pip install --user --no-warn-script-location --upgrade awscli
+# test -x /usr/bin/pip && sudo /usr/bin/pip install --upgrade pip
+# test -x /usr/bin/pip2 && sudo /usr/bin/pip2 install --upgrade pip
 bash ~/setup/al2-aws-ssm.sh
 
 echo "---Update system---"
