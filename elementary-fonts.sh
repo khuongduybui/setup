37af38830a06e1fefb222d60154aa7a1285b78e0
Duy K. Bui <duy@buifamily.info>
2020-12-07 20:56:36 -0500

    feat: Elementary Tweaks

--- elementary-fonts.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-sudo apt install -y dconf-tools
-dconf-editor --I-understand-that-changing-options-can-break-applications org.gnome.desktop.interface font-name
-dconf-editor --I-understand-that-changing-options-can-break-applications org.gnome.desktop.interface document-font-name
-dconf-editor --I-understand-that-changing-options-can-break-applications org.gnome.desktop.interface monospace-font-name
--- elementary.sh
+++ elementary.sh
@@ -5,3 +5,8 @@ sudo apt install -y elementary-sdk
 sudo apt install -y docker.io
 sudo apt install -y direnv
 sudo apt install -y pass
+
+sudo apt install -y software-common-properties
+sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
+sudo apt install -y elementary-tweaks
+
