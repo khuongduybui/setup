cec90e51672fc0d51f214cc3e49d60aefa66df86
Duy K. Bui <duy@buifamily.info>
2020-12-14 21:20:03 -0500

    fix: typo in linux-docker-pass.sh

--- linux-docker-pass.sh
+++ linux-docker-pass.sh
@@ -3,7 +3,7 @@
 # From https://github.com/docker/docker-credential-helpers/releases
 cd /tmp
 rm -rf docker-credential-pass*
-curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest | jq -r ".assets[] | select(.name | test(\"docker-credential-pass\")) | .browser_download_url" | xargs -q -O ./docker-credential-pass.tar.gz
+curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest | jq -r ".assets[] | select(.name | test(\"docker-credential-pass\")) | .browser_download_url" | xargs wget -q -O ./docker-credential-pass.tar.gz
 tar -xzf docker-credential-pass.tar.gz
 mv docker-credential-pass ~/bin
 chmod +x ~/bin/docker-credential-pass
