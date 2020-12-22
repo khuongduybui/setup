32018c331d123080a53270387f3203dd2cd9b503
Duy K. Bui <duy@buifamily.info>
2020-12-22 16:20:54 -0500

    Update docker-init.fish

--- fish-functions/docker-init.fish
+++ fish-functions/docker-init.fish
@@ -1,10 +1,11 @@
 # Defined in /tmp/fish.YZ2zuw/docker-init.fish @ line 2
 function docker-init
-    set -gx DOCKER_BUILDKIT 1
-    find ~/.docker -empty -name config.json >/dev/null; and echo '{}' >~/.docker/config.json
-    if ps aux | grep -q [d]ocker-desktop-proxy
-    else
-        info 'Launching Docker Desktop Proxy'
-        nohup sudo /wsl/docker-desktop/docker-desktop-proxy --distro-name $WSL_DISTRO_NAME --docker-desktop-root /wsl/docker-desktop >(mktemp) &
+    if type -q docker
+        set -gx DOCKER_BUILDKIT 1
+        find ~/.docker -empty -name config.json >/dev/null 2>&1; and echo '{}' >~/.docker/config.json
+        if test -x /wsl/docker-desktop/docker-desktop-proxy; and not ps aux | grep -q [d]ocker-desktop-proxy
+            info 'Launching Docker Desktop Proxy'
+            nohup sudo /wsl/docker-desktop/docker-desktop-proxy --distro-name $WSL_DISTRO_NAME --docker-desktop-root /wsl/docker-desktop >(mktemp) 2>&1 &
+        end
     end
 end
