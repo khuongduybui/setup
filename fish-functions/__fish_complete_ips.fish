5889c6697eaaa36be172aeee8392df20feae7665
Duy K. Bui <duy@buifamily.info>
2021-01-06 09:40:38 -0500

    feat: more precise in ips completion targets

--- fish-functions/__fish_complete_ips.fish
+++ fish-functions/__fish_complete_ips.fish
@@ -1,5 +1,5 @@
 function __fish_complete_ips
     if test -e ~/.aws/config
-        grep ~/.aws/config -e profile | sed 's/\[profile //' | sed 's/]//'
+        grep ~/.aws/config -e '\[profile' | sed 's/\[profile //' | sed 's/]//'
     end
 end
