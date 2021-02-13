b9648f7837d5ab4e041de7417201bb3a911099b8
Duy K. Bui <duy@buifamily.info>
2021-02-13 13:10:16 -0500

    feat: update surface kernel and wps installation

--- debian-surface.sh
+++ debian-surface.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 
-sudo apt install -y git curl wget sed
-test -e ~/linux-surface || git clone --depth 1 https://github.com/jakeday/linux-surface.git ~/linux-surface
-cd ~/linux-surface
-git pull
-sudo bash setup.sh
+wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
+  | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg
+echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" \
+  | sudo tee /etc/apt/sources.list.d/linux-surface.list
 
+sudo apt install -y linux-image-surface linux-headers-surface iptsd libwacom-surface
+sudo systemctl enable iptsd
+
+sudo apt install -y linux-image-surface linux-headers-surface iptsd libwacom-surface linux-surface-secureboot-mok
+sudo update-grub
+
+# sudo apt install -y git curl wget sed
+# test -e ~/linux-surface || git clone --depth 1 https://github.com/jakeday/linux-surface.git ~/linux-surface
+# cd ~/linux-surface
+# git pull
+# sudo bash setup.sh
+
+uname -a
+
+echo https://github.com/linux-surface/linux-surface/wiki/Installation-and-Setup#post-installation
--- debian-wps.sh
+++ debian-wps.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-# From http://wps-community.org/downloads
-wget http://kdl.cc.ksosoft.com/wps-community/download/6758/wps-office_10.1.0.6758_amd64.deb -O /tmp/wps.deb
-sudo apt install -y xdg-utils
-sudo apt install -y /tmp/wps.deb
+# # From http://wps-community.org/downloads
+# wget http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9505/wps-office_11.1.0.9505.XA_amd64.deb -O /tmp/wps.deb
+# sudo apt install -y xdg-utils
+# sudo apt install -y /tmp/wps.deb
+
+echo sudo snap install wps-2019-snap
+echo sudo snap connect wps-2019-snap:cups-control :cups-control
+echo sudo snap connect wps-2019-snap:alsa :alsa
+echo sudo snap connect wps-2019-snap:pulseaudio :pulseaudio
+echo sudo snap connect wps-2019-snap:removable-media :removable-media
