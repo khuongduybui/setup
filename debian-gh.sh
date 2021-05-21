d7ed99599201d56a568c5529599eede028e5ca63
Duy K. Bui <duy@buifamily.info>
2021-05-21 13:29:14 -0400

    feat: fix debian-gh.sh

--- debian-gh.sh
+++ debian-gh.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-sudo apt install -y dirmngr software-properties-common
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh
