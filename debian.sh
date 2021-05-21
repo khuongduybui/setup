b50b1439d5bfcc2a91bd9bcdf3f8b8712b2f6bfe
Duy K. Bui <duy@buifamily.info>
2021-05-21 13:22:21 -0400

    feat: install zoxide by default in Debian-based systems

--- debian.sh
+++ debian.sh
@@ -8,7 +8,7 @@ sudo apt install -y fish
 
 sudo apt install -y neovim
 
-sudo apt install -y vim byobu grc rsync jq direnv fzf
+sudo apt install -y vim byobu grc rsync jq direnv fzf zoxide
 sudo apt install -y mc screenfetch
 
 echo "--- Install dev tools ---"
