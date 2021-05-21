83cb2b9980b543f0ceb57dc78be04b9fc89770a9
Duy K. Bui <duy@buifamily.info>
2021-05-21 00:58:29 -0400

    feat: use sublime-text if available

--- shell-utils/utils.ts
+++ shell-utils/utils.ts
@@ -18,6 +18,7 @@ export const executable = async (command: string) => {
 };
 
 export const editor = async (options = { wait: true }) => {
+  if (await executable("subl")) return ["subl", options.wait ? "-w" : ""];
   if (await executable("io.elementary.code")) return ["io.elementary.code"];
   if (await executable("micro")) return ["micro"];
   if (await executable("code")) return ["code", "-r", options.wait ? "-w" : ""];
