0f7d9598abb28c114772a171d35fc2c5cc2178a2
Duy K. Bui <duy@buifamily.info>
2021-07-26 17:18:04 -0400

    fix: vf-init

--- fish-functions/vf-init.fish
+++ fish-functions/vf-init.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.kKYJLI/vf-init.fish @ line 2
 function vf-init
-    vf new -p (asdf which python) --connect (basename (dirname (pwd)))-(basename (pwd))
+    vf new -p (which python) --connect (basename (dirname (pwd)))-(basename (pwd))
 end
