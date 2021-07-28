aad1ed51417ce7b75b8935397fc97d2a3ccf69c3
Duy K. Bui <duy@buifamily.info>
2021-07-28 11:18:46 -0400

    feat: simpler execute_bit

--- fish-functions/execute_bit.fish
+++ fish-functions/execute_bit.fish
@@ -1,8 +1,17 @@
-# Defined in /tmp/fish.63scBh/execute_bit.fish @ line 2
 function execute_bit
-    segment white magenta (__fish_git_prompt)' '
-    segment white blue ' '(basename (pwd))' '
-    segment_close
+    if type -q starship
+        starship prompt
+    else
+        set_color cyan
+        echo -n (basename (pwd))
+        set_color normal
+        echo -n ' on '
+        set_color magenta
+        echo -n ' '(git branch | grep '*' | sed -e 's/* //')
+        set_color green
+        echo -n '❯ '
+        set_color normal
+    end
 
     echo -n git' '
     set_color cyan
