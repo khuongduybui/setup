67e8103c3e7e355dbfd9ac159afb73cf62df4151
Duy K. Bui <duy@buifamily.info>
2021-08-24 00:23:51 -0400

    feat: update linux-aws-sam-cdk if existing

--- linux-aws-sam-cdk.sh
+++ linux-aws-sam-cdk.sh
@@ -3,4 +3,4 @@
 cd "$(mktemp -d)" || exit
 wget https://github.com/aws/aws-sam-cli/releases/download/sam-cli-beta-cdk/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip
-sudo ./install
+sudo ./install --update
