38093888943b0209fa2d242842a1eb73f8f65f03
Duy K. Bui <duy@buifamily.info>
2021-08-30 14:00:36 -0400

    feat: better sam installation script

--- linux-aws-sam.sh
+++ linux-aws-sam.sh
@@ -4,4 +4,4 @@ cd "$(mktemp -d)" || exit
 # From https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install-linux.html
 wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
-sudo ./sam-installation/install
+sudo ./sam-installation/install --update
