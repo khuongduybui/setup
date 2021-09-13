28d770c4cc65e8048f0da1ea59553cccc7d7b1c7
Duy K. Bui <duy@buifamily.info>
2021-09-13 14:21:44 +0000

    fix: use patheon-tweaks for Elementary OS

--- elementary.sh
+++ elementary.sh
@@ -6,9 +6,8 @@ sudo apt install -y docker.io
 sudo apt install -y direnv
 sudo apt install -y pass
 
-sudo apt install -y software-common-properties
-sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
-sudo apt install -y elementary-tweaks
+sudo add-apt-repository -y ppa:philip.scott/pantheon-tweaks
+sudo apt install -y pantheon-tweaks
 
 sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
