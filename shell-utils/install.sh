de85a82ede6f0fac7831b7116d36fad23debd2a2
Duy K. Bui <duy@buifamily.info>
2021-10-06 00:10:31 -0400

    fix: typo in shell-utils/install.sh

--- shell-utils/install.sh
+++ shell-utils/install.sh
@@ -2,5 +2,5 @@
 
 cd ~/setup/shell-utils || exit
 for script in scripts/*.ts; do
-    deno install -f --no-check -A --unstable "$(pwd)/scripts/$script"
+    deno install -f --no-check -A --unstable "$(pwd)/$script"
 done
