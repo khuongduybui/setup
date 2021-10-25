c8b39d9f54e2aa22b6fa2c85612509722f4efb1e
Duy K. Bui <duy@buifamily.info>
2021-10-25 00:24:18 -0400

    feat: prefer musl in rusty tools

--- linux-exa.sh
+++ linux-exa.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 cd $(mktemp -d)
-curl -s https://api.github.com/repos/ogham/exa/releases/latest | jq -r ".assets[] | select(.name | test(\"linux-x86_64\")) | .browser_download_url" | xargs wget -q -O ./exa.zip
+curl -s https://api.github.com/repos/ogham/exa/releases/latest | jq -r ".assets[] | select(.name | test(\"linux-x86_64-musl\")) | .browser_download_url" | xargs wget -q -O ./exa.zip
 unzip exa.zip
 rm -f ~/bin/exa
 mv bin/exa ~/bin/exa
