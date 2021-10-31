ce3253883bc56573165debb044c376e78f621a07
Duy K. Bui <duy@buifamily.info>
2021-10-31 12:44:35 -0400

    feat: set themes for both bat and git-delta

--- config.fish
+++ config.fish
@@ -15,7 +15,7 @@ if status --is-interactive
     ## AWS
     # type -q aws-sso-util; and aws-sso-util login --all --headless &
 
-    __is_day; and git config --global delta.syntax-theme "Monokai Extended Light"; or git config --global delta.syntax-theme "Monokai Extended"
+    __is_day; and set -xU BAT_THEME "Monokai Extended Light"; or set -xU BAT_THEME "Monokai Extended"
 end
 
 if test -z $INIT
