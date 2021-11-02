28358b336c7e3dae38f2fba645bf2bdbec0d7212
Duy K. Bui <duy@buifamily.info>
2021-11-02 19:54:14 -0400

    feat: remove obsolete powershell bats

--- powershell.bat
+++
@@ -1,1 +0,0 @@
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted -NoProfile -NoExit -File %USERPROFILE%\setup\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
--- powershell6.bat
+++
@@ -1,1 +0,0 @@
-"%PROGRAMFILES%\PowerShell\6\pwsh.exe" -ExecutionPolicy Unrestricted -NoProfile -NoExit -File %USERPROFILE%\setup\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
