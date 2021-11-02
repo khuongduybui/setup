11cbcb14fe65065b66f5e57ada169145f3380c7e
Duy K. Bui <duy@buifamily.info>
2021-11-02 13:33:43 -0400

    feat: debian-edge.sh now defaults to stable channel

--- debian-edge.sh
+++ debian-edge.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-wget "https://go.microsoft.com/fwlink/?linkid=2124602" -O /tmp/edge.deb
+# wget "https://go.microsoft.com/fwlink/?linkid=2124602" -O /tmp/edge.deb
+wget "https://go.microsoft.com/fwlink/?linkid=2149051" -O /tmp/edge.deb
 sudo gdebi /tmp/edge.deb
