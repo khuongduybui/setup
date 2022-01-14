cfcb62baed87ac083acfc1c6e1d6f20bd1802721
Duy K. Bui <duy@buifamily.info>
2022-01-14 09:57:29 -0500

    feat: no need to confirm in wsl-down

--- fish-functions/wsl-down.fish
+++ fish-functions/wsl-down.fish
@@ -1,14 +1,12 @@
-# Defined in /tmp/fish.3m9FIy/wsl-down.fish @ line 2
+# Defined in /tmp/fish.Tf8Tzh/wsl-down.fish @ line 2
 function wsl-down
-	if __read_confirm --title 'Shut down'
-        if test -z $argv[1]
-            if test -z $WSL_DISTRO_NAME
-                wsl.exe --shutdown
-            else
-                wsl.exe --terminate $WSL_DISTRO_NAME
-            end
+    if test -z $argv[1]
+        if test -z $WSL_DISTRO_NAME
+            wsl.exe --shutdown
         else
-            wsl.exe --terminate $argv[1]
+            wsl.exe --terminate $WSL_DISTRO_NAME
         end
+    else
+        wsl.exe --terminate $argv[1]
     end
 end
