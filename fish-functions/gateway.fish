df81ec2d316d9ddaaee626a66db82a6d5e800a02
Duy K. Bui <duy@buifamily.info>
2022-03-16 12:16:19 -0400

    feat: add gateway fish function

---
+++ fish-functions/gateway.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.YP00el/gateway.fish @ line 1
+function gateway
+    x-www-browser https://gateway1.hawk.activeeye.com/gateway
+    x-www-browser https://gateway1.wasp.activeeye.com/gateway
+    x-www-browser https://gateway1.lion.activeeye.com/gateway
+end
