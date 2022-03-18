5f71dcfe37bdb494eaa49535003ffab1160aba7f
Duy K. Bui <duy@buifamily.info>
2022-03-18 13:37:49 -0400

    feat: add orca gateway

--- fish-functions/msi-init.fish
+++ fish-functions/msi-init.fish
@@ -1,10 +1,11 @@
-# Defined in /tmp/fish.idrruf/msi-init.fish @ line 1
+# Defined in /tmp/fish.iiHIU9/msi-init.fish @ line 2
 function msi-init
     aws-sso-util login --all
 
     x-www-browser https://gateway1.hawk.activeeye.com/gateway
     x-www-browser https://gateway1.wasp.activeeye.com/gateway
     x-www-browser https://gateway1.lion.activeeye.com/gateway
+    x-www-browser https://gateway1.orca.activeeye.com/gateway
 
     sft list-servers
 end
