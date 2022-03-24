57271dee2b335f0244d98d70e4a89256dbe6a9e2
Duy K. Bui <duy@buifamily.info>
2022-03-23 21:54:33 -0400

    feat: add ibex into msi-init

--- fish-functions/msi-init.fish
+++ fish-functions/msi-init.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.iiHIU9/msi-init.fish @ line 2
+# Defined in /tmp/fish.z3qxcl/msi-init.fish @ line 2
 function msi-init
     aws-sso-util login --all
 
@@ -6,6 +6,7 @@ function msi-init
     x-www-browser https://gateway1.wasp.activeeye.com/gateway
     x-www-browser https://gateway1.lion.activeeye.com/gateway
     x-www-browser https://gateway1.orca.activeeye.com/gateway
+    x-www-browser https://gateway1.ibex.activeeye.com/gateway
 
     sft list-servers
 end
