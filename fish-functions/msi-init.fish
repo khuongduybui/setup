9a1d220f2391578ca2d6f2b3f578be95a8b84c81
Duy K. Bui <duy@buifamily.info>
2022-04-01 10:03:53 -0400

    feat: oops ibex is gone

--- fish-functions/msi-init.fish
+++ fish-functions/msi-init.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.pThgH7/msi-init.fish @ line 2
+# Defined in /tmp/fish.Vzqxp5/msi-init.fish @ line 2
 function msi-init
     aws-sso-util login --all
 
@@ -10,8 +10,8 @@ function msi-init
     x-www-browser https://gateway1.lion.activeeye.com/gateway
     echo https://gateway1.orca.activeeye.com/gateway
     x-www-browser https://gateway1.orca.activeeye.com/gateway
-    echo https://gateway1.ibex.activeeye.com/gateway
-    x-www-browser https://gateway1.ibex.activeeye.com/gateway
+    # echo https://gateway1.ibex.activeeye.com/gateway
+    # x-www-browser https://gateway1.ibex.activeeye.com/gateway
     echo https://gateway1.wolf.activeeye.com/gateway
     x-www-browser https://gateway1.wolf.activeeye.com/gateway
 
