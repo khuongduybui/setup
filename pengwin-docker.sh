2e1055957ee2a8406277e6bc461567a5f2a04184
Duy K. Bui <duy@buifamily.info>
2022-04-26 09:57:29 -0400

    fix: syntax error in pengwin-docker

--- pengwin-docker.sh
+++ pengwin-docker.sh
@@ -5,4 +5,4 @@ brew install docker-compose
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 sudo service docker start
-sudo usermod -aG docker (whoami)
+sudo usermod -aG docker "$(whoami)"
